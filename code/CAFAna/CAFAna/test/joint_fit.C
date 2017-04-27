0a057cf87934bfc639117fad1e905ea94f80574c
Christopher Backhouse <bckhouse@fnal.gov>
2017-04-27 15:06:59 -0500

    Macro working out all the goodies so far.

---
+++ code/CAFAna/CAFAna/test/joint_fit.C
@@ -0,0 +1,271 @@
+#ifdef __CINT__
+void joint_fit(bool reload = false){}
+#else
+
+#include "CAFAna/Core/SpectrumLoader.h"
+#include "CAFAna/Core/Spectrum.h"
+#include "CAFAna/Cuts/TruthCuts.h"
+
+#include "CAFAna/Systs/DUNEXSecSysts.h"
+#include "CAFAna/Core/SystShifts.h"
+#include "CAFAna/Experiment/SingleSampleExperiment.h"
+#include "CAFAna/Experiment/MultiExperiment.h"
+#include "CAFAna/Analysis/Calcs.h"
+#include "CAFAna/Analysis/Fit.h"
+#include "CAFAna/Vars/FitVars.h"
+
+using namespace ana;
+
+#include "Utilities/rootlogon.C"
+
+#include "StandardRecord/StandardRecord.h"
+
+#include "TCanvas.h"
+#include "TH1.h"
+#include "TLegend.h"
+
+const Var kRecoE = SIMPLEVAR(dune.Ev_reco);
+const Var kPIDmu = SIMPLEVAR(dune.numu_pid);
+const Var kPIDe = SIMPLEVAR(dune.nue_pid);
+const Var kPIDFD = SIMPLEVAR(dune.mvaresult);
+const Var kQ = SIMPLEVAR(dune.reco_q);
+
+const HistAxis axis("Reconstructed energy (GeV)",
+                    Binning::Simple(40, 0, 10),
+                    kRecoE);
+
+// One year
+const double potND = 1.47e21;
+// POT/yr * 3.5yrs * mass correction
+const double potFD = 3.5 * 1.47e21 * 40/1.13;
+
+void Legend(const std::string& title)
+{
+  TH1* dummy = new TH1F("", "", 1, 0, 1);
+
+  TLegend* leg = new TLegend(.65, .65, .85, .85);
+  leg->SetFillStyle(0);
+  leg->AddEntry((TObject*)0, ("#bf{"+title+"}").c_str(), "");
+  dummy->SetLineColor(kRed);
+  leg->AddEntry(dummy->Clone(), "Fake data", "lep");
+  dummy->SetLineColor(kBlack);
+  leg->AddEntry(dummy->Clone(), "Nominal MC", "l");
+  dummy->SetLineColor(kBlue);
+  leg->AddEntry(dummy->Clone(), "Best fit", "l");
+  leg->Draw();
+}
+
+void joint_fit(bool reload = false)
+{
+  rootlogon(); // style
+
+  if(reload || TFile("joint_fit_state.root").IsZombie()){
+    SpectrumLoader loaderNDFHC("/dune/data/users/marshalc/NDTF_FGT_FHC.root");
+    SpectrumLoader loaderNDRHC("/dune/data/users/marshalc/NDTF_FGT_RHC.root");
+
+    auto* loaderNDFHCPOT = loaderNDFHC.LoaderForRunPOT(1);
+    auto* loaderNDRHCPOT = loaderNDRHC.LoaderForRunPOT(1);
+
+    SpectrumLoader loaderFDNumuFHC("/pnfs/dune/persistent/TaskForce_AnaTree/far/train/v2.1/numutest.root");
+    SpectrumLoader loaderFDNueFHC("/pnfs/dune/persistent/TaskForce_AnaTree/far/train/v2.1/nuetest.root");
+
+    auto* loaderFDNumuFHCBeam  = loaderFDNumuFHC.LoaderForRunPOT(20000001);
+    auto* loaderFDNumuFHCNue   = loaderFDNumuFHC.LoaderForRunPOT(20000002);
+    auto* loaderFDNumuFHCNuTau = loaderFDNumuFHC.LoaderForRunPOT(20000003);
+    auto* loaderFDNumuFHCNC    = loaderFDNumuFHC.LoaderForRunPOT(0);
+
+    auto* loaderFDNueFHCBeam  = loaderFDNueFHC.LoaderForRunPOT(20000001);
+    auto* loaderFDNueFHCNue   = loaderFDNueFHC.LoaderForRunPOT(20000002);
+    auto* loaderFDNueFHCNuTau = loaderFDNueFHC.LoaderForRunPOT(20000003);
+    auto* loaderFDNueFHCNC    = loaderFDNueFHC.LoaderForRunPOT(0);
+
+    SpectrumLoader loaderFDNumuRHC("/pnfs/dune/persistent/TaskForce_AnaTree/far/train/v2.1/anumutest.root");
+    SpectrumLoader loaderFDNueRHC("/pnfs/dune/persistent/TaskForce_AnaTree/far/train/v2.1/anuetest.root");
+
+    auto* loaderFDNumuRHCBeam  = loaderFDNumuRHC.LoaderForRunPOT(20000004);
+    auto* loaderFDNumuRHCNue   = loaderFDNumuRHC.LoaderForRunPOT(20000005);
+    auto* loaderFDNumuRHCNuTau = loaderFDNumuRHC.LoaderForRunPOT(20000006);
+    auto* loaderFDNumuRHCNC    = loaderFDNumuRHC.LoaderForRunPOT(0);
+    
+    auto* loaderFDNueRHCBeam  = loaderFDNueRHC.LoaderForRunPOT(20000004);
+    auto* loaderFDNueRHCNue   = loaderFDNueRHC.LoaderForRunPOT(20000005);
+    auto* loaderFDNueRHCNuTau = loaderFDNueRHC.LoaderForRunPOT(20000006);
+    auto* loaderFDNueRHCNC    = loaderFDNueRHC.LoaderForRunPOT(0);
+
+    PredictionScaleComp predNDFHC(*loaderNDFHCPOT, 
+                                  axis,
+                                  kPIDmu > 0.5 && kQ < 0.,
+                                  GetDUNEXSecSysts());
+
+    PredictionScaleComp predNDRHC(*loaderNDRHCPOT, 
+                                  axis,
+                                  kPIDmu > 0.5 && kQ > 0.,
+                                  GetDUNEXSecSysts());
+
+    // 0.8 is a random guess at a cut position - should be studied
+    PredictionScaleComp predFDNumuFHC(*loaderFDNumuFHCBeam, 
+                                      *loaderFDNumuFHCNue,
+                                      *loaderFDNumuFHCNuTau,
+                                      *loaderFDNumuFHCNC,
+                                      axis,
+                                      kPIDFD > 0.8,
+                                      GetDUNEXSecSysts());
+
+    PredictionScaleComp predFDNueFHC(*loaderFDNueFHCBeam, 
+                                     *loaderFDNueFHCNue,
+                                     *loaderFDNueFHCNuTau,
+                                     *loaderFDNueFHCNC,
+                                     axis,
+                                     kPIDFD > 0.8,
+                                     GetDUNEXSecSysts());
+
+    PredictionScaleComp predFDNumuRHC(*loaderFDNumuRHCBeam, 
+                                      *loaderFDNumuRHCNue,
+                                      *loaderFDNumuRHCNuTau,
+                                      *loaderFDNumuRHCNC,
+                                      axis,
+                                      kPIDFD > 0.8,
+                                      GetDUNEXSecSysts());
+
+    PredictionScaleComp predFDNueRHC(*loaderFDNueRHCBeam, 
+                                     *loaderFDNueRHCNue,
+                                     *loaderFDNueRHCNuTau,
+                                     *loaderFDNueRHCNC,
+                                     axis,
+                                     kPIDFD > 0.8,
+                                     GetDUNEXSecSysts());
+
+    loaderNDFHC.Go();
+    loaderNDRHC.Go();
+    loaderFDNumuFHC.Go();
+    loaderFDNueFHC.Go();
+    loaderFDNumuRHC.Go();
+    loaderFDNueRHC.Go();
+
+    TFile fout("joint_fit_state.root", "RECREATE");
+    predNDFHC.SaveTo(fout.mkdir("nd_fhc"));
+    predNDRHC.SaveTo(fout.mkdir("nd_rhc"));
+    predFDNumuFHC.SaveTo(fout.mkdir("fd_numu_fhc"));
+    predFDNueFHC.SaveTo(fout.mkdir("fd_nue_fhc"));
+    predFDNumuRHC.SaveTo(fout.mkdir("fd_numu_rhc"));
+    predFDNueRHC.SaveTo(fout.mkdir("fd_nue_rhc"));
+    std::cout << "Saved state to joint_fit_state.root" << std::endl;
+  }
+  else{
+    std::cout << "Loading state from joint_fit_state.root" << std::endl;
+  }
+
+  TFile fin("joint_fit_state.root");
+  PredictionScaleComp& predNDFHC = *ana::LoadFrom<PredictionScaleComp>(fin.GetDirectory("nd_fhc")).release();
+  PredictionScaleComp& predNDRHC = *ana::LoadFrom<PredictionScaleComp>(fin.GetDirectory("nd_rhc")).release();
+  PredictionScaleComp& predFDNumuFHC = *ana::LoadFrom<PredictionScaleComp>(fin.GetDirectory("fd_numu_fhc")).release();
+  PredictionScaleComp& predFDNueFHC = *ana::LoadFrom<PredictionScaleComp>(fin.GetDirectory("fd_nue_fhc")).release();
+  PredictionScaleComp& predFDNumuRHC = *ana::LoadFrom<PredictionScaleComp>(fin.GetDirectory("fd_numu_rhc")).release();
+  PredictionScaleComp& predFDNueRHC = *ana::LoadFrom<PredictionScaleComp>(fin.GetDirectory("fd_nue_rhc")).release();
+  fin.Close();
+
+  // What systematic parameters will we shift in the fake data?
+  DUNEXSecSyst nushift(nu_MEC_dummy);
+  DUNEXSecSyst nubarshift(nubar_MEC_dummy);
+  std::map<const ISyst*,double> shiftmap;
+  shiftmap.emplace(&nushift, +1.);
+  shiftmap.emplace(&nubarshift, +1.);
+  SystShifts shifts(shiftmap);
+
+  // Make some ND Asimov fake data
+  Spectrum fakeNDFHC = predNDFHC.PredictSyst(0, shifts).FakeData(potND);
+  Spectrum fakeNDRHC = predNDRHC.PredictSyst(0, shifts).FakeData(potND);
+
+  // Make matching FD Asimov fake data, plus some oscillations
+  osc::IOscCalculatorAdjustable* inputOsc = DefaultOscCalc();
+  inputOsc->SetdCP(1.5*TMath::Pi());
+
+  Spectrum fakeFDNumuFHC = predFDNumuFHC.PredictSyst(inputOsc, shifts).FakeData(potFD);
+  Spectrum fakeFDNueFHC = predFDNueFHC.PredictSyst(inputOsc, shifts).FakeData(potFD);
+
+  Spectrum fakeFDNumuRHC = predFDNumuRHC.PredictSyst(inputOsc, shifts).FakeData(potFD);
+  Spectrum fakeFDNueRHC = predFDNueRHC.PredictSyst(inputOsc, shifts).FakeData(potFD);
+
+  SingleSampleExperiment exptNDFHC(&predNDFHC, fakeNDFHC);
+  SingleSampleExperiment exptNDRHC(&predNDRHC, fakeNDRHC);
+
+  SingleSampleExperiment exptFDNumuFHC(&predFDNumuFHC, fakeFDNumuFHC);
+  SingleSampleExperiment exptFDNueFHC(&predFDNueFHC, fakeFDNueFHC);
+
+  SingleSampleExperiment exptFDNumuRHC(&predFDNumuRHC, fakeFDNumuRHC);
+  SingleSampleExperiment exptFDNueRHC(&predFDNueRHC, fakeFDNueRHC);
+
+  // Joint fit between ND and FD and the covariance matrix
+  MultiExperiment expt({&exptNDFHC, &exptNDRHC,
+                        &exptFDNumuFHC, &exptFDNueFHC,
+                        &exptFDNumuRHC, &exptFDNueRHC,
+                        new DUNEXSecCorrelation
+                       });
+
+  const std::vector<const IFitVar*> oscFitVars = {&kFitSinSqTheta23,
+                                                  &kFitDmSq32Scaled,
+                                                  &kFitSinSq2Theta13,
+                                                  &kFitDeltaInPiUnits};
+
+  // Use everything - slow
+  //  const std::vector<const ISyst*> systFitVars = GetDUNEXSecSysts();
+  // Fit a reduced list of variables of interest
+  const std::vector<const ISyst*> systFitVars = {GetDUNEXSecSyst(nu_MEC_dummy),
+                                                 GetDUNEXSecSyst(nubar_MEC_dummy)};
+
+  // Set up the fit
+  Fitter fit(&expt, oscFitVars, systFitVars);
+  // Where will we start our search?
+  osc::IOscCalculatorAdjustable* oscSeed = DefaultOscCalc();
+  SystShifts systSeed = SystShifts::Nominal();
+  // Do the fit - updates the "seed" variables with the best fit point
+  fit.Fit(oscSeed, systSeed);
+
+  // Best fit spectra for all the samples
+  Spectrum bfNDFHC = predNDFHC.PredictSyst(0, systSeed);
+  Spectrum bfNDRHC = predNDRHC.PredictSyst(0, systSeed);
+  Spectrum bfFDNumuFHC = predFDNumuFHC.PredictSyst(oscSeed, systSeed);
+  Spectrum bfFDNueFHC = predFDNueFHC.PredictSyst(oscSeed, systSeed);
+  Spectrum bfFDNumuRHC = predFDNumuRHC.PredictSyst(oscSeed, systSeed);
+  Spectrum bfFDNueRHC = predFDNueRHC.PredictSyst(oscSeed, systSeed);
+
+  // Future work - show the numu/nue/NC sub-components of these
+
+  new TCanvas;
+  fakeNDFHC.ToTH1(potND, kRed)->Draw("ep");
+  predNDFHC.Predict(0).ToTH1(potND)->Draw("hist same");
+  bfNDFHC.ToTH1(potND, kBlue)->Draw("hist same");
+  Legend("ND FHC");
+
+  new TCanvas;
+  fakeNDRHC.ToTH1(potND, kRed)->Draw("ep");
+  predNDRHC.Predict(0).ToTH1(potND)->Draw("hist same");
+  bfNDRHC.ToTH1(potND, kBlue)->Draw("hist same");
+  Legend("ND RHC");
+
+  new TCanvas;
+  fakeFDNumuFHC.ToTH1(potFD, kRed)->Draw("ep");
+  predFDNumuFHC.Predict(oscSeed).ToTH1(potFD)->Draw("hist same");
+  bfFDNumuFHC.ToTH1(potFD, kBlue)->Draw("hist same");
+  Legend("FD FHC #nu_{#mu}");
+
+  new TCanvas;
+  fakeFDNueFHC.ToTH1(potFD, kRed)->Draw("ep");
+  predFDNueFHC.Predict(oscSeed).ToTH1(potFD)->Draw("hist same");
+  bfFDNueFHC.ToTH1(potFD, kBlue)->Draw("hist same");
+  Legend("FD FHC #nu_{e}");
+
+  new TCanvas;
+  fakeFDNumuRHC.ToTH1(potFD, kRed)->Draw("ep");
+  predFDNumuRHC.Predict(oscSeed).ToTH1(potFD)->Draw("hist same");
+  bfFDNumuRHC.ToTH1(potFD, kBlue)->Draw("hist same");
+  Legend("FD RHC #nu_{#mu}");
+
+  new TCanvas;
+  fakeFDNueRHC.ToTH1(potFD, kRed)->Draw("ep");
+  predFDNueRHC.Predict(oscSeed).ToTH1(potFD)->Draw("hist same");
+  bfFDNueRHC.ToTH1(potFD, kBlue)->Draw("hist same");
+  Legend("FD RHC #nu_{e}");
+}
+
+#endif
