efba5a50b9350abf7ff9af6bd55381d07e2cc4f7
Christopher Backhouse <bckhouse@fnal.gov>
2017-08-08 18:54:09 -0500

    Fitter torture tests 2a and 2b. Really need loops here :(

--- code/CAFAna/CAFAna/test/fitter_validation.C
+++ code/CAFAna/CAFAna/test/fitter_validation.C
@@ -5,6 +5,8 @@ void fitter_validation(bool reload = false){}
 #include "CAFAna/Core/SpectrumLoader.h"
 #include "CAFAna/Core/LoadFromFile.h"
 #include "CAFAna/Prediction/PredictionNoExtrap.h"
+#include "CAFAna/Systs/DUNEFluxSysts.h"
+#include "CAFAna/Systs/Systs.h"
 
 using namespace ana;
 
@@ -150,6 +152,81 @@ void fitter_validation(bool reload = false)
                                     axis,
                                     kPIDFD > 0.8);
 
+
+    // Numbers are after sections in Dan's document
+    const SystShifts shifts2a(&kNCSyst, +1);
+
+    PredictionNoExtrap predFDNumuFHC2a(*loaderFDNumuFHCBeam, 
+                                       *loaderFDNumuFHCNue,
+                                       *loaderFDNumuFHCNuTau,
+                                       *loaderFDNumuFHCNC,
+                                       axis,
+                                       kPIDFD > 0.8,
+                                       shifts2a);
+
+    PredictionNoExtrap predFDNueFHC2a(*loaderFDNueFHCBeam, 
+                                      *loaderFDNueFHCNue,
+                                      *loaderFDNueFHCNuTau,
+                                      *loaderFDNueFHCNC,
+                                      axis,
+                                      kPIDFD > 0.8,
+                                      shifts2a);
+
+    PredictionNoExtrap predFDNumuRHC2a(*loaderFDNumuRHCBeam, 
+                                       *loaderFDNumuRHCNue,
+                                       *loaderFDNumuRHCNuTau,
+                                       *loaderFDNumuRHCNC,
+                                       axis,
+                                       kPIDFD > 0.8,
+                                       shifts2a);
+
+    PredictionNoExtrap predFDNueRHC2a(*loaderFDNueRHCBeam, 
+                                      *loaderFDNueRHCNue,
+                                      *loaderFDNueRHCNuTau,
+                                      *loaderFDNueRHCNC,
+                                      axis,
+                                      kPIDFD > 0.8,
+                                      shifts2a);
+
+    // TODO - this isn't exactly Dan's prescription
+    DUNEFluxSystVector fsv = GetDUNEFluxSysts(256);
+    SystShifts shifts2b;
+    gRandom->SetSeed(42);
+    for(const ISyst* s: fsv) shifts2b.SetShift(s, gRandom->Gaus(0, 1));
+
+    PredictionNoExtrap predFDNumuFHC2b(*loaderFDNumuFHCBeam, 
+                                       *loaderFDNumuFHCNue,
+                                       *loaderFDNumuFHCNuTau,
+                                       *loaderFDNumuFHCNC,
+                                       axis,
+                                       kPIDFD > 0.8,
+                                       shifts2b);
+
+    PredictionNoExtrap predFDNueFHC2b(*loaderFDNueFHCBeam, 
+                                      *loaderFDNueFHCNue,
+                                      *loaderFDNueFHCNuTau,
+                                      *loaderFDNueFHCNC,
+                                      axis,
+                                      kPIDFD > 0.8,
+                                      shifts2b);
+
+    PredictionNoExtrap predFDNumuRHC2b(*loaderFDNumuRHCBeam, 
+                                       *loaderFDNumuRHCNue,
+                                       *loaderFDNumuRHCNuTau,
+                                       *loaderFDNumuRHCNC,
+                                       axis,
+                                       kPIDFD > 0.8,
+                                       shifts2b);
+
+    PredictionNoExtrap predFDNueRHC2b(*loaderFDNueRHCBeam, 
+                                      *loaderFDNueRHCNue,
+                                      *loaderFDNueRHCNuTau,
+                                      *loaderFDNueRHCNC,
+                                      axis,
+                                      kPIDFD > 0.8,
+                                      shifts2b);
+
+
     loaderFDNumuFHC.Go();
     loaderFDNueFHC.Go();
     loaderFDNumuRHC.Go();
@@ -160,6 +237,17 @@ void fitter_validation(bool reload = false)
     predFDNueFHC.SaveTo(fout.mkdir("fd_nue_fhc"));
     predFDNumuRHC.SaveTo(fout.mkdir("fd_numu_rhc"));
     predFDNueRHC.SaveTo(fout.mkdir("fd_nue_rhc"));
+
+    predFDNumuFHC2a.SaveTo(fout.mkdir("fd_numu_fhc_2a"));
+    predFDNueFHC2a.SaveTo(fout.mkdir("fd_nue_fhc_2a"));
+    predFDNumuRHC2a.SaveTo(fout.mkdir("fd_numu_rhc_2a"));
+    predFDNueRHC2a.SaveTo(fout.mkdir("fd_nue_rhc_2a"));
+
+    predFDNumuFHC2b.SaveTo(fout.mkdir("fd_numu_fhc_2b"));
+    predFDNueFHC2b.SaveTo(fout.mkdir("fd_nue_fhc_2b"));
+    predFDNumuRHC2b.SaveTo(fout.mkdir("fd_numu_rhc_2b"));
+    predFDNueRHC2b.SaveTo(fout.mkdir("fd_nue_rhc_2b"));
+
     std::cout << "Saved state to " << stateFname << std::endl;
   }
   else{
@@ -171,6 +259,16 @@ void fitter_validation(bool reload = false)
   PredictionNoExtrap& predFDNueFHC = *ana::LoadFrom<PredictionNoExtrap>(fin.GetDirectory("fd_nue_fhc")).release();
   PredictionNoExtrap& predFDNumuRHC = *ana::LoadFrom<PredictionNoExtrap>(fin.GetDirectory("fd_numu_rhc")).release();
   PredictionNoExtrap& predFDNueRHC = *ana::LoadFrom<PredictionNoExtrap>(fin.GetDirectory("fd_nue_rhc")).release();
+
+  IPrediction* predFDNumuFHC2a = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_numu_fhc_2a")).release();
+  IPrediction* predFDNueFHC2a = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_nue_fhc_2a")).release();
+  IPrediction* predFDNumuRHC2a = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_numu_rhc_2a")).release();
+  IPrediction* predFDNueRHC2a = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_nue_rhc_2a")).release();
+
+  IPrediction* predFDNumuFHC2b = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_numu_fhc_2b")).release();
+  IPrediction* predFDNueFHC2b = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_nue_fhc_2b")).release();
+  IPrediction* predFDNumuRHC2b = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_numu_rhc_2b")).release();
+  IPrediction* predFDNueRHC2b = ana::LoadFrom<IPrediction>(fin.GetDirectory("fd_nue_rhc_2b")).release();
   fin.Close();
   std::cout << "Done loading state" << std::endl;
 
@@ -210,6 +308,29 @@ void fitter_validation(bool reload = false)
       hnuerhc->Write(("nue_rhc_"+hieStr+"_"+dcpStr).c_str());
     } // end for delta
   } // end for hie
+
+  osc::IOscCalculatorAdjustable* inputOsc = NuFitOscCalc(+1);
+  inputOsc->SetdCP(0);
+
+  TH1* hnumufhc2a = predFDNumuFHC2a->Predict(inputOsc).ToTH1(potFD);
+  TH1* hnuefhc2a = predFDNueFHC2a->Predict(inputOsc).ToTH1(potFD);
+  TH1* hnumurhc2a = predFDNumuFHC2a->Predict(inputOsc).ToTH1(potFD);
+  TH1* hnuerhc2a = predFDNueFHC2a->Predict(inputOsc).ToTH1(potFD);
+
+  hnumufhc2a->Write("numu_fhc_2a");
+  hnuefhc2a->Write("nue_fhc_2a");
+  hnumurhc2a->Write("numu_rhc_2a");
+  hnuerhc2a->Write("nue_rhc_2a");
+
+  TH1* hnumufhc2b = predFDNumuFHC2b->Predict(inputOsc).ToTH1(potFD);
+  TH1* hnuefhc2b = predFDNueFHC2b->Predict(inputOsc).ToTH1(potFD);
+  TH1* hnumurhc2b = predFDNumuFHC2b->Predict(inputOsc).ToTH1(potFD);
+  TH1* hnuerhc2b = predFDNueFHC2b->Predict(inputOsc).ToTH1(potFD);
+
+  hnumufhc2b->Write("numu_fhc_2b");
+  hnuefhc2b->Write("nue_fhc_2b");
+  hnumurhc2b->Write("numu_rhc_2b");
+  hnuerhc2b->Write("nue_rhc_2b");
 
   std::cout << "Wrote " << outputFname << std::endl;
 }
