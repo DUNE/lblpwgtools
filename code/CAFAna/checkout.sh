fba2feec0033c8693e67df87801d0f0d66724081
Christopher Backhouse <bckhouse@fnal.gov>
2018-03-20 06:39:45 -0500

    Switch over to CVMFS based software (thanks Seb). Now any SL6 machine with CVMFS should be able to use CAFAna.

--- code/CAFAna/checkout.sh
+++ code/CAFAna/checkout.sh
@@ -35,9 +35,8 @@ chmod +x update-release || exit 1
 
 ./update-release -rel development || exit 1
 
-
-source setup/setup_nova.sh -b maxopt -6 $SRT_DIST || exit 1
-
+export CVMFS_DISTRO_BASE=/cvmfs/nova.opensciencegrid.org/ || exit 1
+source setup/setup_nova.sh -b maxopt -6 $SRT_DIST -e $CVMFS_DISTRO_BASE/externals/ || exit 1
 
 ### Editing of a few problematic packages ###
 
--- code/CAFAna/setup.sh
+++ code/CAFAna/setup.sh
@@ -8,6 +8,7 @@ fi
 
 source srt/srt.sh
 
-source setup/setup_nova.sh -b maxopt -6 $SRT_DIST
+export CVMFS_DISTRO_BASE=/cvmfs/nova.opensciencegrid.org/ || exit 1
+source setup/setup_nova.sh -b maxopt -6 $SRT_DIST -e $CVMFS_DISTRO_BASE/externals/ || exit 1
 
 cd releases/development/CAFAna/
