a878809af0010bce18e3bb3f658772ad6bd65b42
Seb Jones <jonesseb100@gmail.com>
2018-11-16 17:32:22 -0600

    Created a place for ND reconstruction systematics

---
+++ code/CAFAna/CAFAna/Systs/NDRecoSysts.h
@@ -0,0 +1,16 @@
+// NDRecoSysts.h
+// Systematics to simulate reconstruction systematics in the ND
+#pragma once
+
+#include "CAFAna/Core/ISyst.h"
+#include "StandardRecord/StandardRecord.h"
+#include "CAFAna/Core/Utilities.h"
+
+#include "TFile.h"
+#include "TH1.h"
+#include "TRandom3.h"
+
+namespace ana {
+
+
+}
