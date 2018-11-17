f03624439d0d06f7263420aa570bbe03116cef04
Chris Backhouse <cjbackhouse@gmail.com>
2018-11-17 10:42:00 -0600

    Make backtraces work better.

--- code/CAFAna/CAFAna/Core/DebugHelpers.cxx
+++ code/CAFAna/CAFAna/Core/DebugHelpers.cxx
@@ -65,7 +65,7 @@ namespace ana
       if(f){
         std::string ss;
         f >> ss;
-        if(ss.size() < 8 || &ss[ss.size()-8] != std::string("root.exe")) return;
+        if(ss.find("root.exe") == std::string::npos) return;
       }
 
       // Handle uncaught exceptions
