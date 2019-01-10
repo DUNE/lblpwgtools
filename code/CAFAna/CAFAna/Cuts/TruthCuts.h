f60d55747eba40a73b4ed3ad3f5c2e78bb3ddeef
Christopher Marshall <marshall@pas.rochester.edu>
2019-01-10 11:01:41 -0600

    cut away from ND module boundaries in z also. remove events on ArCLight planes and module walls

--- code/CAFAna/CAFAna/Cuts/TruthCuts.h
+++ code/CAFAna/CAFAna/Cuts/TruthCuts.h
@@ -71,7 +71,7 @@ namespace ana
                         }
                         else{
                           bool inDeadRegion = false;
-                          for( int i = -3; i < 4; ++i ) {
+                          for( int i = -3; i <= 3; ++i ) {
                             // 0.5cm cathode in the middle of each module, plus 0.5cm buffer
                             double cathode_center = i*102.1;
                             if( sr->dune.vtx_x > cathode_center - 0.75 && sr->dune.vtx_x < cathode_center + 0.75 ) inDeadRegion = true;
@@ -79,7 +79,18 @@ namespace ana
                             // 1.6cm dead region between modules (0.5cm module wall and 0.3cm pixel plane, x2)
                             // don't worry about outer boundary because events are only generated in active Ar + insides
                             double module_boundary = i*102.1 + 51.05;
-                            if( i < 3 && sr->dune.vtx_x > module_boundary - 1.3 && sr->dune.vtx_x < module_boundary + 1.3 ) inDeadRegion = true;
+                            if( i <= 2 && sr->dune.vtx_x > module_boundary - 1.3 && sr->dune.vtx_x < module_boundary + 1.3 ) inDeadRegion = true;
+                          }
+                          for( int i = 1; i <= 4; ++i ) {
+                            // module boundaries in z are 1.8cm (0.4cm ArCLight plane + 0.5cm module wall, x2)
+                            // module is 102.1cm wide, but only 101.8cm long due to cathode (0.5cm) being absent in length
+                            // but ArCLight is 0.1cm thicker than pixel plane so it's 0.3cm difference
+                            // positions are off-set by 0.6 because I defined 0 to be the upstream edge based on the active volume
+                            // by inspecting a plot, and aparently missed by 3 mm, but whatever
+                            // add 8mm = 2 pad buffer due to worse position resolution in spatial dimension z compared to timing direction x
+                            // so total FV gap will be 1.8 + 2*0.8 = 3.4cm
+                            double module_boundary = i*101.8 - 0.6;
+                            if( sr->dune.vtx_z > module_boundary - 1.7 && sr->dune.vtx_z < module_boundary + 1.7 ) inDeadRegion = true;
                           }
 
                           return (
