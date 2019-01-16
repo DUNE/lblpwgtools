150bc48cacca570ef332ee3c021c6ba7902e7128
wilkinson-nu <callumw@fnal.gov>
2019-01-16 10:27:38 -0600

    Update fitting scripts to use new god function

--- code/CAFAna/CAFAna/scripts/cpv_joint.C
+++ code/CAFAna/CAFAna/scripts/cpv_joint.C
@@ -1,13 +1,8 @@
-// #ifdef __CINT__
-// void cpv_joint(bool reload = false){}
-// #else
-
 #include "common_fit_definitions.C"
 
-// Need to accept filename, ND/FD, systs and reload as arguments
-void cpv_joint(std::string stateFname="common_state_mcc11v2.root",
-	       std::string outputFname="cpv_sens_ndfd_allsyst.root",
-	       std::string systSet = "flux", bool useND=true){
+void cpv_joint(std::string stateFname="common_state_mcc11v3.root",
+	       std::string outputFname="cpv_sens_ndfd_nosyst.root",
+	       std::string systSet = "nosyst", bool useND=true){
   
   gROOT->SetBatch(1);
 
@@ -23,7 +18,6 @@ void cpv_joint(std::string stateFname="common_state_mcc11v2.root",
   fout->cd();
   for(int hie = -1; hie <= +1; hie += 2){
 
-    const std::string hieStr = (hie > 0) ? "nh" : "ih";
     double dcpstep = 2*TMath::Pi()/36;
     TGraph* gCPV = new TGraph();
     double thisdcp;
@@ -42,27 +36,31 @@ void cpv_joint(std::string stateFname="common_state_mcc11v2.root",
       
       // Still need to loop over dcp choices
       for (int idcp = 0; idcp < 2; ++idcp) {
-	
-	osc::IOscCalculatorAdjustable* testOsc = NuFitOscCalc(hie);	
 	double dcptest = idcp*TMath::Pi();
-	testOsc->SetdCP(dcptest);
-	
-	// Make a map of seed points to try (replaces the old loops)
-	std::map<const IFitVar*, std::vector<double>> oscSeeds = {};
-	oscSeeds[&kFitSinSqTheta23] = {testOsc->GetTh23(), TMath::Pi()/2 - testOsc->GetTh23()}; // try both octants
-	oscSeeds[&kFitDmSq32Scaled] = {testOsc->GetDmsq32()*1000., -1*testOsc->GetDmsq32()*1000.}; // try both hierarchies
-	
-	Penalizer_GlbLike penalty(hie);
-	SystShifts trueSyst = kNoShift;
-	SystShifts testSyst = kNoShift;
-	
-	thischisq = RunFitPoint(stateFname, useND ? pot_nd : 0, useND ? pot_nd : 0, pot_fd, pot_fd,
-				trueOsc, trueSyst, false,
-				oscVars, systlist,
-				testOsc, testSyst,
-				oscSeeds, &penalty);
-	
-	chisqmin = TMath::Min(thischisq,chisqmin);
+
+	for(int ihie = -1; ihie <= +1; ihie += 2) {
+
+	  for (int ioct = -1; ioct <= 1; ioct +=2) {
+
+	    osc::IOscCalculatorAdjustable* testOsc = NuFitOscCalc(ihie, ioct);	
+	    testOsc->SetdCP(dcptest);
+	  
+	    // Make a map of seed points to try (replaces the old loops)
+	    std::map<const IFitVar*, std::vector<double>> oscSeeds = {};
+	    
+	    Penalizer_GlbLike penalty(ihie, ioct);
+	    SystShifts trueSyst = kNoShift;
+	    SystShifts testSyst = kNoShift;
+	    
+	    thischisq = RunFitPoint(stateFname, (useND) ? pot_nd : 0, (useND) ? pot_nd : 0, pot_fd, pot_fd,
+				    trueOsc, trueSyst, false,
+				    oscVars, systlist,
+				    testOsc, testSyst,
+				    oscSeeds, &penalty);
+	    
+	    chisqmin = TMath::Min(thischisq,chisqmin);
+	  }
+	}
       }
 
       chisqmin = TMath::Max(chisqmin,1e-6);      
@@ -76,4 +74,3 @@ void cpv_joint(std::string stateFname="common_state_mcc11v2.root",
   fout->Close();
 }
 
-//#endif
