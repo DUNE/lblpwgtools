53b4d5e299f2ad865cd9148995b30b5f5f512182
Luke Pickering <luketpickering@googlemail.com>
2019-03-10 10:44:13 -0500

    Uses snprintf rather than inflexible homespun replacer.

--- code/CAFAna/CAFAna/scripts/MakePredInterps.C
+++ code/CAFAna/CAFAna/scripts/MakePredInterps.C
@@ -1,51 +1,13 @@
 #include "common_fit_definitions.C"
 
-inline std::string str_replace(std::string const &inp, std::string const &from,
-                               std::string const &to) {
-  std::stringstream ss("");
-
-  size_t nextOccurence = 0;
-  size_t prevOccurence = 0;
-  bool AtEnd = false;
-  while (!AtEnd) {
-    nextOccurence = inp.find(from, prevOccurence);
-    if (nextOccurence == std::string::npos) {
-      if (prevOccurence == inp.length()) {
-        break;
-      }
-      AtEnd = true;
-    }
-    if ((nextOccurence != prevOccurence) || (nextOccurence == 0)) {
-      ss << inp.substr(prevOccurence, (nextOccurence - prevOccurence));
-      if (!AtEnd) {
-        ss << to;
-      }
-    }
-    prevOccurence = nextOccurence + from.size();
-  }
-  return ss.str();
-}
-
-template <typename T> inline T str2T(std::string const &str) {
-  std::istringstream stream(str);
-  T d;
-  stream >> d;
-
-  if (stream.fail()) {
-    std::cerr << "[WARN]: Failed to parse string: " << str
-              << " as requested type." << std::endl;
-    return T();
-  }
-
-  return d;
-}
+#include <cstdio>
 
 int main(int argc, char const *argv[]) {
 
   if ((argc < 2) || (std::string(argv[1]) == "-?") ||
       (std::string(argv[1]) == "--help") || (argc < 6)) {
     std::cout << "[RUNLIKE]: " << argv[0]
-              << " <Sample Type> </path/to/file_%N.root> <NStart> <NLast> "
+              << " <Sample Type> </path/to/file_%d.root> <NStart> <NLast> "
                  "<OutputFileName> [<AxesBlobName>]"
               << std::endl;
     return 0;
@@ -53,8 +15,8 @@ int main(int argc, char const *argv[]) {
 
   SampleType stype = GetSampleType(argv[1]);
   std::string file_name_fmt = argv[2];
-  size_t NStart = str2T<size_t>(argv[3]);
-  size_t NLast = str2T<size_t>(argv[4]);
+  size_t NStart = std::stol(argv[3]);
+  size_t NLast = std::stol(argv[4]);
   std::string output_file_name = argv[5];
 
   std::string AxisBlobName = "default";
@@ -67,7 +29,9 @@ int main(int argc, char const *argv[]) {
   std::vector<std::string> file_list;
 
   for (size_t i = NStart; i <= NLast; ++i) {
-    file_list.push_back(str_replace(file_name_fmt, "%N", std::to_string(i)));
+    char buf[1000];
+    snprintf(buf, 1000, file_name_fmt.c_str(), i);
+    file_list.push_back(buf);
     std::cout << "[INFO]: Will read " << file_list.back() << std::endl;
   }
 
