0799fc6dc7b215eb5adf4f802169769bc3fffac5
Luke Pickering <luketpickering@googlemail.com>
2019-05-23 18:24:27 +0000

    Updates configure_and_build script.

--- code/CAFAna/CAFAna/standalone_configure_and_build.sh
+++ code/CAFAna/CAFAna/standalone_configure_and_build.sh
@@ -5,6 +5,8 @@ USE_UPS="0"
 CORES=1
 USE_GPERF=0
 CMAKE_BUILD_TYPE=DEBUG
+INSTALL_DIR=""
+USE_KNL="0"
 
 while [[ ${#} -gt 0 ]]; do
 
@@ -35,6 +37,17 @@ while [[ ${#} -gt 0 ]]; do
       echo "[OPT]: Will compile release build type."
       ;;
 
+      --rdb)
+
+      CMAKE_BUILD_TYPE="RELWITHDEBINFO"
+      echo "[OPT]: Will compile release with debug build type."
+      ;;
+
+      --knl)
+
+      USE_KNL="1"
+      echo "[OPT]: Will compile for KNL arch."
+      ;;
 
       -j|--n-cores)
 
@@ -48,6 +61,18 @@ while [[ ${#} -gt 0 ]]; do
       shift # past argument
       ;;
 
+      -I|--install-to)
+
+      if [[ ${#} -lt 2 ]]; then
+        echo "[ERROR]: ${1} expected a value."
+        exit 1
+      fi
+
+      INSTALL_DIR="$2"
+      echo "[OPT]: Will install to \"${INSTALL_DIR}\"."
+      shift # past argument
+      ;;
+
       -?|--help)
       echo "[RUNLIKE] ${SCRIPTNAME}"
       echo -e "\t-f|--force-remove      : Remove previous build directory if it exists."
@@ -56,6 +81,7 @@ while [[ ${#} -gt 0 ]]; do
 
       echo -e "\t-u|--use-UPS           : Try and use ups to set up required packages, rather than assuming they exist on the local system."
       echo -e "\t-j|--n-cores           : Number of cores to pass to make install."
+      echo -e "\t-I|--install-to        : Directory to install to."
       echo -e "\t-?|--help              : Print this message."
       exit 0
       ;;
@@ -84,8 +110,8 @@ cd build
 mkdir Ext
 cd Ext
 
-svn checkout http://cdcvs.fnal.gov/subversion/novaart.pkgs.svn/trunk/OscLib
-svn checkout http://cdcvs.fnal.gov/subversion/novaart.pkgs.svn/trunk/Utilities
+svn checkout https://cdcvs.fnal.gov/subversion/novaart.pkgs.svn/trunk/OscLib
+svn checkout https://cdcvs.fnal.gov/subversion/novaart.pkgs.svn/trunk/Utilities
 
 cd ../
 
@@ -118,5 +144,5 @@ else
 
 fi
 
-cmake ../ -DSRC_ROOT_PARENT=$(readlink -f ../../) -DUSED_UPS=${USE_UPS} -DUSE_GPERFTOOLS=${USE_GPERF} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+cmake ../ -DSRC_ROOT_PARENT=$(readlink -f ../../) -DUSED_UPS=${USE_UPS} -DUSE_GPERFTOOLS=${USE_GPERF} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DKNL=${USE_KNL}
 make install -j ${CORES}
