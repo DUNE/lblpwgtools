69b66718e6de4efd261f0956ab7e8dd34eb1eb93
Seb Jones <jonesseb100@gmail.com>
2019-05-28 13:28:55 -0500

    Added checks to correct negative particle energies

--- code/CAFAna/CAFAna/Systs/EnergySysts.h
+++ code/CAFAna/CAFAna/Systs/EnergySysts.h
@@ -30,6 +30,7 @@ namespace ana
 
       double scale = .02 * sigma;
       if (sr->dune.isFD) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
 	sr->dune.Ev_reco_numu  += scale * sr->dune.RecoHadEnNumu;
 	sr->dune.Ev_reco_nue   += scale * sr->dune.RecoHadEnNue;
 	sr->dune.RecoHadEnNumu *= 1. + scale;
@@ -60,6 +61,7 @@ namespace ana
 		  sr->dune.RecoLepEnNue);
 	const double scale = .01 * sigma;
 	if (sr->dune.isFD) {
+	  if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
 	  sr->dune.Ev_reco_numu  += scale*pow(sr->dune.RecoHadEnNumu, 0.5)*sr->dune.RecoHadEnNumu;
 	  sr->dune.Ev_reco_nue   += scale*pow(sr->dune.RecoHadEnNue, 0.5)*sr->dune.RecoHadEnNue;
 	  sr->dune.RecoHadEnNumu += scale*pow(sr->dune.RecoHadEnNumu, 0.5)*sr->dune.RecoHadEnNumu;
@@ -87,6 +89,7 @@ namespace ana
 		  sr->dune.RecoHadEnNue,
 		  sr->dune.RecoLepEnNue);
       if (sr->dune.isFD) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
 	const double scale = .02 * sigma;
 	sr->dune.Ev_reco_numu += sr->dune.RecoHadEnNumu*scale*pow(sr->dune.RecoHadEnNumu+0.1, -0.5);
 	sr->dune.Ev_reco_nue  += sr->dune.RecoHadEnNue*scale*pow(sr->dune.RecoHadEnNue+0.1, -0.5);
@@ -117,9 +120,16 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_numu,
 		  sr->dune.Ev_reco_nue,
 		  sr->dune.RecoHadEnNumu,
-		  sr->dune.RecoHadEnNue);
+		  sr->dune.RecoHadEnNue,
+		  sr->dune.eRecoP,
+		  sr->dune.eRecoPim,
+		  sr->dune.eRecoPip);
+      const double scale = .05 * sigma;
       if (sr->dune.isFD) {
-	const double scale = .05 * sigma;
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoP < 0) sr->dune.eRecoP = 0.;
+	if (sr->dune.eRecoPim < 0) sr->dune.eRecoPim = 0.;
+	if (sr->dune.eRecoPip < 0) sr->dune.eRecoPip = 0.;
 	double sumE = sr->dune.eRecoP + sr->dune.eRecoPip + sr->dune.eRecoPim;
 	sr->dune.Ev_reco_numu += sumE * scale * pow(sumE, 0.5);
 	sr->dune.Ev_reco_nue  += sumE * scale * pow(sumE, 0.5);
@@ -141,9 +151,16 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_numu,
 		  sr->dune.Ev_reco_nue,
 		  sr->dune.RecoHadEnNumu,
-		  sr->dune.RecoHadEnNue);
+		  sr->dune.RecoHadEnNue,
+		  sr->dune.eRecoP,
+		  sr->dune.eRecoPim,
+		  sr->dune.eRecoPip);
+      const double scale = .05 * sigma;
       if (sr->dune.isFD) {
-	const double scale = .05 * sigma;
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoP < 0) sr->dune.eRecoP = 0.;
+	if (sr->dune.eRecoPim < 0) sr->dune.eRecoPim = 0.;
+	if (sr->dune.eRecoPip < 0) sr->dune.eRecoPip = 0.;
 	double sumE = sr->dune.eRecoP + sr->dune.eRecoPip + sr->dune.eRecoPim;
 	sr->dune.Ev_reco_numu += sumE * scale * pow(sumE+0.1, -0.5);
 	sr->dune.Ev_reco_nue  += sumE * scale * pow(sumE+0.1, -0.5);
@@ -169,8 +186,8 @@ namespace ana
 		  sr->dune.RecoLepEnNumu);
       if (sr->dune.isFD && sr->dune.isCC==1 && abs(sr->dune.nuPDG)==14) {
 	const double scale = .005 * sigma;
-	sr->dune.Ev_reco_numu  += sr->dune.RecoLepEnNumu*sr->dune.RecoLepEnNumu*scale;
-	sr->dune.RecoLepEnNumu += sr->dune.RecoLepEnNumu*sr->dune.RecoLepEnNumu*scale;
+	sr->dune.Ev_reco_numu  += sr->dune.RecoLepEnNumu * pow(sr->dune.RecoLepEnNumu, 0.5) * scale;
+	sr->dune.RecoLepEnNumu += sr->dune.RecoLepEnNumu * pow(sr->dune.RecoLepEnNumu, 0.5) * scale;
       }
     }
   };
@@ -211,9 +228,12 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_numu,
 		  sr->dune.Ev_reco_nue,
 		  sr->dune.RecoHadEnNumu,
-		  sr->dune.RecoHadEnNue);
-      if (sr->dune.isFD) {
-	const double scale = .3 * sigma;
+		  sr->dune.RecoHadEnNue,
+		  sr->dune.eRecoN);
+      const double scale = .3 * sigma;
+      if (sr->dune.isFD && sr->dune.eRecoN > 0) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoN < 0) sr->dune.eRecoN = 0.;
 	sr->dune.Ev_reco_numu += sr->dune.eRecoN * pow(sr->dune.eRecoN, 0.5) * scale;
 	sr->dune.Ev_reco_nue  += sr->dune.eRecoN * pow(sr->dune.eRecoN, 0.5) * scale;
 	sr->dune.RecoHadEnNumu += sr->dune.eRecoN * pow(sr->dune.eRecoN, 0.5) * scale;
@@ -234,9 +254,12 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_numu,
 		  sr->dune.Ev_reco_nue,
 		  sr->dune.RecoHadEnNumu,
-		  sr->dune.RecoHadEnNue);
+		  sr->dune.RecoHadEnNue,
+		  sr->dune.eRecoN);
+      const double scale = .3 * sigma;
       if (sr->dune.isFD) {
-	const double scale = .3 * sigma;
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoN < 0) sr->dune.eRecoN = 0.;
 	sr->dune.Ev_reco_numu += sr->dune.eRecoN * scale * pow(sr->dune.eRecoN+0.1, -0.5);
 	sr->dune.Ev_reco_nue  += sr->dune.eRecoN * scale * pow(sr->dune.eRecoN+0.1, -0.5);
 	sr->dune.RecoHadEnNumu += sr->dune.eRecoN * scale * pow(sr->dune.eRecoN+0.1, -0.5);
@@ -261,9 +284,12 @@ namespace ana
 		  sr->dune.Ev_reco_nue,
 		  sr->dune.RecoHadEnNumu,
 		  sr->dune.RecoHadEnNue,
-		  sr->dune.RecoLepEnNue);
+		  sr->dune.RecoLepEnNue,
+		  sr->dune.eRecoPi0);
+      const double scale = .025 * sigma;
       if (sr->dune.isFD) {
-	const double scale = .025 * sigma;
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoPi0 < 0) sr->dune.eRecoPi0 = 0.;
 	sr->dune.Ev_reco_numu += sr->dune.eRecoPi0 * pow(sr->dune.eRecoPi0, 0.5) * scale;
 	sr->dune.Ev_reco_nue  += sr->dune.eRecoPi0 * pow(sr->dune.eRecoPi0, 0.5) * scale;
 	sr->dune.RecoHadEnNumu += sr->dune.eRecoPi0 * pow(sr->dune.eRecoPi0, 0.5) * scale;
@@ -290,9 +316,12 @@ namespace ana
 		  sr->dune.RecoHadEnNumu,
 		  sr->dune.RecoHadEnNue,
 		  sr->dune.RecoLepEnNumu,
-		  sr->dune.RecoLepEnNue);
+		  sr->dune.RecoLepEnNue,
+		  sr->dune.eRecoPi0);
       const double scale = .025 * sigma;
       if (sr->dune.isFD) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoPi0 < 0) sr->dune.eRecoPi0 = 0.;
 	sr->dune.Ev_reco_numu += sr->dune.eRecoPi0 * scale * pow(sr->dune.eRecoPi0+0.1, -0.5);
 	sr->dune.Ev_reco_nue  += sr->dune.eRecoPi0 * scale * pow(sr->dune.eRecoPi0+0.1, -0.5);
 	sr->dune.RecoHadEnNumu += sr->dune.eRecoPi0 * scale * pow(sr->dune.eRecoPi0+0.1, -0.5);
@@ -319,9 +348,7 @@ namespace ana
     {
       restore.Add(sr->dune.Ev_reco_numu,
                   sr->dune.RecoLepEnNumu);
-
       const double scale = .02 * sigma;
-
       // Checks if FD  and select only CC muon neutrino events
       if(sr->dune.isFD && abs(sr->dune.nuPDG) == 14 && sr->dune.isCC == 1){
 	sr->dune.Ev_reco_numu   += sr->dune.RecoLepEnNumu * scale;
@@ -345,11 +372,16 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_nue,
                   sr->dune.Ev_reco_numu,
                   sr->dune.RecoHadEnNumu,
-                  sr->dune.RecoHadEnNue);
-
-      const double scale =  .05*sigma;
-      
+                  sr->dune.RecoHadEnNue,
+		  sr->dune.eRecoP,
+		  sr->dune.eRecoPip,
+		  sr->dune.eRecoPim);
+      const double scale = .05 * sigma;
       if(sr->dune.isFD) { 
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoP < 0) sr->dune.eRecoP = 0.;
+	if (sr->dune.eRecoPim < 0) sr->dune.eRecoPim = 0.;
+	if (sr->dune.eRecoPip < 0) sr->dune.eRecoPip = 0.;
 	const double sumE = sr->dune.eRecoP + sr->dune.eRecoPim + sr->dune.eRecoPip;
 	sr->dune.Ev_reco_numu += sumE * scale;
 	sr->dune.Ev_reco_nue  += sumE * scale;
@@ -375,16 +407,16 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_numu,
                   sr->dune.Ev_reco_nue,
                   sr->dune.RecoHadEnNumu,
-                  sr->dune.RecoHadEnNue);
-
+                  sr->dune.RecoHadEnNue,
+		  sr->dune.eRecoN);
       const double scale = .20 * sigma;
-
       if(sr->dune.isFD) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoN < 0.) sr->dune.eRecoN = 0.;
  	sr->dune.Ev_reco_numu  += sr->dune.eRecoN * scale;
 	sr->dune.Ev_reco_nue   += sr->dune.eRecoN * scale;
 	sr->dune.RecoHadEnNumu += sr->dune.eRecoN * scale;
 	sr->dune.RecoHadEnNue  += sr->dune.eRecoN * scale;
-	
       }
     }
   };
@@ -405,10 +437,12 @@ namespace ana
 		  sr->dune.RecoHadEnNue,
 		  sr->dune.RecoHadEnNumu,
 		  sr->dune.Ev_reco_nue,
-		  sr->dune.Ev_reco_numu);
-
+		  sr->dune.Ev_reco_numu,
+		  sr->dune.eRecoPi0);
       const double scale = 0.025 * sigma;
       if (sr->dune.isFD) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoPi0 < 0.) sr->dune.eRecoPi0 = 0.;
 	sr->dune.Ev_reco_nue  += sr->dune.eRecoPi0 * scale;
 	sr->dune.Ev_reco_numu += sr->dune.eRecoPi0 * scale;
 	sr->dune.RecoHadEnNue  += sr->dune.eRecoPi0 * scale; 
@@ -436,7 +470,6 @@ namespace ana
     {
       restore.Add(sr->dune.Ev_reco_numu,
 		  sr->dune.RecoLepEnNumu);
-
       const double scale = .02*sigma;      
       // FD charged current numus only
       if (sr->dune.isFD && sr->dune.isCC && abs(sr->dune.nuPDG)==14) {
@@ -461,10 +494,12 @@ namespace ana
 		  sr->dune.Ev_reco_numu,
 		  sr->dune.RecoHadEnNumu,
 		  sr->dune.RecoHadEnNue,
-		  sr->dune.RecoLepEnNue);
-
+		  sr->dune.RecoLepEnNue,
+		  sr->dune.eRecoPi0);
       const double scale = .02*sigma;      
       if (sr->dune.isFD){
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoPi0 < 0.) sr->dune.eRecoPi0 = 0.;
 	sr->dune.Ev_reco_nue  += (sr->dune.ePi0 - sr->dune.eRecoPi0) * scale;
 	sr->dune.Ev_reco_numu += (sr->dune.ePi0 - sr->dune.eRecoPi0) * scale;
 	sr->dune.RecoHadEnNumu += (sr->dune.ePi0 - sr->dune.eRecoPi0) * scale;
@@ -491,10 +526,16 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_nue,
 		  sr->dune.Ev_reco_numu,
 		  sr->dune.RecoHadEnNue,
-		  sr->dune.RecoHadEnNumu);
-
+		  sr->dune.RecoHadEnNumu,
+		  sr->dune.eRecoP,
+		  sr->dune.eRecoPip,
+		  sr->dune.eRecoPim);
       const double scale = .02*sigma;      
       if (sr->dune.isFD) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoP < 0.) sr->dune.eRecoP = 0.;
+	if (sr->dune.eRecoPip < 0.) sr->dune.eRecoPip = 0.;
+	if (sr->dune.eRecoPim < 0.) sr->dune.eRecoPim = 0.;
 	const double trueSum = sr->dune.ePip + sr->dune.ePim + sr->dune.eP;
 	const double recoSum = sr->dune.eRecoPip + sr->dune.eRecoPim + sr->dune.eRecoP;
 	sr->dune.Ev_reco_nue   += (trueSum - recoSum) * scale;
@@ -519,11 +560,13 @@ namespace ana
       restore.Add(sr->dune.Ev_reco_nue,
 		  sr->dune.Ev_reco_numu,
 		  sr->dune.RecoHadEnNue,
-		  sr->dune.RecoHadEnNumu);
-
+		  sr->dune.RecoHadEnNumu,
+		  sr->dune.eRecoN);
       const double scale = .1*sigma;      
       // FD charged current numus only
       if (sr->dune.isFD) {
+	if (sr->dune.RecoHadEnNue < 0.) sr->dune.RecoHadEnNue = 0.;
+	if (sr->dune.eRecoN < 0.) sr->dune.eRecoN = 0.;
 	sr->dune.Ev_reco_nue   += (sr->dune.eN - sr->dune.eRecoN) * scale;
 	sr->dune.Ev_reco_numu  += (sr->dune.eN - sr->dune.eRecoN) * scale;
 	sr->dune.RecoHadEnNue  += (sr->dune.eN - sr->dune.eRecoN) * scale;
