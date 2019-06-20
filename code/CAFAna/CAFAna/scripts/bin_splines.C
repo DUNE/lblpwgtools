ebf2311a338478aa39117283feb2e8d9055eff57
wilkinson-nu <callumw@fnal.gov>
2019-06-20 14:24:19 -0500

    uSiNg NaMeSpAcE aNa

--- code/CAFAna/CAFAna/scripts/bin_splines.C
+++ code/CAFAna/CAFAna/scripts/bin_splines.C
@@ -1,5 +1,7 @@
 #include "CAFAna/Analysis/common_fit_definitions.h"
 
+using namespace ana;
+
 void bin_splines(std::string stateFname="common_state_mcc11v3.root",
 		 std::string outputFname="bin_splines_mcc11v3.root"){
   
