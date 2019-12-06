5e2fa029089e6d4efb6cd09e79e8edef854d4561
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2019-12-06 09:55:00 -0500

    centralize typedefs for stuff that will be templated over stan::math::var here.
    this commit contains some references to things that haven't been defined yet, so don't expect it to compile.
    they'll be introduced in the subsequent commits.

---
+++ CAFAna/Core/StanTypedefs.h
@@ -0,0 +1,52 @@
+///  StanTypedefs.h:
+///    Typedefs of various types templated over stan::math::var,
+///    centralized here for convenience.
+///    They don't get put in the header files associated with the types themselves
+///    because there's an annoying chain of declarations needed for the typedef'ing,
+///    and it's much easier to maintain if it's in a single place.
+#pragma once
+
+namespace stan
+{
+  namespace math
+  {
+    class var;
+  }
+}
+
+namespace osc
+{
+  template <typename T> class _IOscCalculatorAdjustable;
+  typedef _IOscCalculatorAdjustable<stan::math::var> IOscCalculatorAdjustableStan;
+
+  template <typename T> class _IOscCalculator;
+  typedef _IOscCalculator<stan::math::var> IOscCalculatorStan;
+
+  template <typename T> class _OscCalculatorPMNS;
+  typedef _OscCalculatorPMNS<stan::math::var> OscCalculatorPMNSStan;
+
+  template <typename T> class _OscCalculatorPMNSOpt;
+  typedef _OscCalculatorPMNSOpt<stan::math::var> OscCalculatorPMNSOptStan;
+
+}
+
+namespace ana
+{
+  // note: typedefs over forward-declared types are fragile.
+  // if either the underlying type changes, its forward declaration
+  // needs to be updated here.
+
+  // ---------------------
+  // vars
+  template <typename T> class _IFitVar;
+  typedef _IFitVar<stan::math::var> IFitVarStan;
+
+  template <typename T> class _IConstrainedFitVar;
+  typedef _IConstrainedFitVar<stan::math::var> IConstrainedFitVarStan;
+
+  // ---------------------
+  // experiments
+  template <typename T> class _ILkhdExperiment;
+  typedef _ILkhdExperiment<stan::math::var> ILkhdExperiment;
+
+}
