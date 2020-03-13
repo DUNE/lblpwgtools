eb979b0b00407ec5bf25c30f9ae2a6eddf20f5cb
Diana Patricia Mendez <dmendezme@bnl.gov>
2020-03-13 15:33:01 -0500

    A little bit of POT ordering here.

--- CAFAna/Analysis/common_fit_definitions.cxx
+++ CAFAna/Analysis/common_fit_definitions.cxx
@@ -59,11 +59,10 @@ using namespace ana;
 
 unsigned gRNGSeed = 0;
 
-// POT for 3.5 years
-const double pot_fd = 3.5 * POT120 * 40 / 1.13;
-const double pot_nd = 3.5 * POT120;
-// This is pretty annoying, but the above is for 7 years staged, which is 336 kT
-// MW yr
+// Avoid fixing number of years                                                                                                                        
+const double pot_fd = POT120 * 40 / 1.13;
+const double pot_nd = POT120;
+// The above is for 7 years staged which is 336 kT MW yr
 const double nom_exposure = 336.;
 
 size_t NFluxParametersToAddToStatefile = 30;
@@ -743,17 +742,19 @@ void ParseDataSamples(std::string cmdLineInput, double &pot_nd_fhc,
 
   double exposure_ratio = exposure / nom_exposure;
 
-  // Now sort out which samples to include
-  pot_nd_fhc = pot_nd_rhc = pot_fd_fhc_nue = pot_fd_rhc_nue = pot_fd_fhc_numu =
-      pot_fd_rhc_numu = 0;
+  // Now sort out which samples to include                                                                                                             
+  pot_nd_fhc = pot_nd_rhc = 0;
+  pot_fd_fhc_nue = pot_fd_fhc_numu = 0;
+  pot_fd_rhc_nue = pot_fd_rhc_numu = 0;
 
-  // Hacky McHackerson is here to stay!
+  // Hacky McHackerson is here to stay!                                                                                                                
   if (input.find("nd") != std::string::npos) {
-    pot_nd_fhc = pot_nd_rhc = pot_nd * exposure_ratio;
+    pot_nd_fhc = pot_nd * exposure_ratio * years_fhc;
+    pot_nd_rhc = pot_nd * exposure_ratio * years_rhc;
   }
   if (input.find("fd") != std::string::npos) {
-    pot_fd_fhc_nue = pot_fd_rhc_nue = pot_fd_fhc_numu = pot_fd_rhc_numu =
-        pot_fd * exposure_ratio;
+    pot_fd_fhc_nue = pot_fd_fhc_numu = pot_fd * exposure_ratio * years_fhc;
+    pot_fd_rhc_nue = pot_fd_rhc_numu = pot_fd * exposure_ratio * years_rhc;
   }
 
   // Now allow specific subsets
