cd032ec3a29c475c2b69a0abf103ebe276319755
Luke Pickering <luketpickering@googlemail.com>
2020-04-30 11:22:30 -0500

    Tweaks Event rate table code to be more flexible with overlapping stops

--- app/OffAxisEventRates.C
+++ app/OffAxisEventRates.C
@@ -15,7 +15,7 @@ using namespace PRISM;
 
 void OffAxisEventRates(fhicl::ParameterSet const &pred) {
 
-std::string const &off_axis_file = pred.get<std::string>("input_file");
+  std::string const &off_axis_file = pred.get<std::string>("input_file");
   std::vector<std::string> const &output_file =
       pred.get<std::vector<std::string>>("output_file");
   std::string const &output_dir = pred.get<std::string>("output_dir", "");
@@ -24,7 +24,7 @@ std::string const &off_axis_file = pred.get<std::string>("input_file");
   auto SpecialRunWeightHist = pred.get<std::pair<std::string, std::string>>(
       "special_run_weight", {"", ""});
 
-                         TH1 *whist = nullptr;
+  TH1 *whist = nullptr;
   if (SpecialRunWeightHist.first.size()) {
     TDirectory *gdc = gDirectory;
     TFile wf(SpecialRunWeightHist.first.c_str());
@@ -39,7 +39,6 @@ std::string const &off_axis_file = pred.get<std::string>("input_file");
       gdc->cd();
     }
   }
-
 
   osc::IOscCalculatorAdjustable *calc = NuFitOscCalc(1);
 
@@ -61,62 +60,64 @@ std::string const &off_axis_file = pred.get<std::string>("input_file");
   Var NDWeight_Eff = GetNDWeight("NDEff");
   Var NDWeight_XSecEff = GetNDWeight("CVXSec NDEff");
 
-    if (whist) { // If we are faking a special run
-      auto specrunweight =
-          ana::Var({}, [&](const caf::StandardRecord *sr) -> double {
-            if (sr->dune.det_x || (sr->dune.vtx_x > 0)) {
-              return 1;
-            }
-            return whist->GetBinContent(
-                whist->GetXaxis()->FindFixBin(sr->dune.Ev));
-          });
-      NDWeight = NDWeight * specrunweight;
-      NDWeight_XSec = NDWeight_XSec * specrunweight;
-      NDWeight_Eff = NDWeight_Eff * specrunweight;
-      NDWeight_XSecEff = NDWeight_XSecEff * specrunweight;
-    }
+  if (whist) { // If we are faking a special run
+    auto specrunweight =
+        ana::Var({}, [&](const caf::StandardRecord *sr) -> double {
+          if (sr->dune.det_x || (sr->dune.vtx_x > 0)) {
+            return 1;
+          }
+          return whist->GetBinContent(
+              whist->GetXaxis()->FindFixBin(sr->dune.Ev));
+        });
+    NDWeight = NDWeight * specrunweight;
+    NDWeight_XSec = NDWeight_XSec * specrunweight;
+    NDWeight_Eff = NDWeight_Eff * specrunweight;
+    NDWeight_XSecEff = NDWeight_XSecEff * specrunweight;
+  }
 
   Spectrum NDSpect_all(NDLoader, axes.XProjection, axes.OffAxisPosition,
                        kNoCut);
   Spectrum NDSpect_signal(NDLoader, axes.XProjection, axes.OffAxisPosition,
-                            NDSignalCut, kNoShift);
-  Spectrum NDSpect_signal_AnaWeighted(NDLoader, axes.XProjection, axes.OffAxisPosition,
-                               NDSignalCut, kNoShift, NDWeight);
+                          NDSignalCut, kNoShift);
+  Spectrum NDSpect_signal_AnaWeighted(NDLoader, axes.XProjection,
+                                      axes.OffAxisPosition, NDSignalCut,
+                                      kNoShift, NDWeight);
   Spectrum NDSpect_signal_CVXSecWeighted(NDLoader, axes.XProjection,
-                                  axes.OffAxisPosition, NDSignalCut, kNoShift,
-                                  NDWeight_XSec);
-  Spectrum NDSpect_signal_EffWeighted(NDLoader, axes.XProjection, axes.OffAxisPosition,
-                               NDSignalCut, kNoShift, NDWeight_Eff);
-  Spectrum NDSpect_signal_AllWeighted(NDLoader, axes.XProjection, axes.OffAxisPosition,
-                               NDSignalCut, kNoShift, NDWeight_XSecEff);
+                                         axes.OffAxisPosition, NDSignalCut,
+                                         kNoShift, NDWeight_XSec);
+  Spectrum NDSpect_signal_EffWeighted(NDLoader, axes.XProjection,
+                                      axes.OffAxisPosition, NDSignalCut,
+                                      kNoShift, NDWeight_Eff);
+  Spectrum NDSpect_signal_AllWeighted(NDLoader, axes.XProjection,
+                                      axes.OffAxisPosition, NDSignalCut,
+                                      kNoShift, NDWeight_XSecEff);
 
   Spectrum NDSpect_select(NDLoader, axes.XProjection, axes.OffAxisPosition,
-                            NDSignalCut_sel, kNoShift);
+                          NDSignalCut_sel, kNoShift);
   Spectrum NDSpect_select_AnaWeighted(NDLoader, axes.XProjection,
                                       axes.OffAxisPosition, NDSignalCut_sel,
                                       kNoShift, NDWeight);
-  Spectrum NDSpect_select_AnaWeighted_Sig(NDLoader, axes.XProjection,
-                                      axes.OffAxisPosition, NDSignalCut_sel_signal,
-                                      kNoShift, NDWeight);
-  Spectrum NDSpect_select_AnaWeighted_WSB(NDLoader, axes.XProjection,
-                                      axes.OffAxisPosition, NDSignalCut_sel_WSB,
-                                      kNoShift, NDWeight);
-  Spectrum NDSpect_select_AnaWeighted_NC(NDLoader, axes.XProjection,
-                                      axes.OffAxisPosition, NDSignalCut_sel_NC,
-                                      kNoShift, NDWeight);
+  Spectrum NDSpect_select_AnaWeighted_Sig(
+      NDLoader, axes.XProjection, axes.OffAxisPosition, NDSignalCut_sel_signal,
+      kNoShift, NDWeight);
+  Spectrum NDSpect_select_AnaWeighted_WSB(
+      NDLoader, axes.XProjection, axes.OffAxisPosition, NDSignalCut_sel_WSB,
+      kNoShift, NDWeight);
+  Spectrum NDSpect_select_AnaWeighted_NC(
+      NDLoader, axes.XProjection, axes.OffAxisPosition, NDSignalCut_sel_NC,
+      kNoShift, NDWeight);
   Spectrum NDSpect_select_CVXSecWeighted(NDLoader, axes.XProjection,
-                                  axes.OffAxisPosition, NDSignalCut_sel, kNoShift,
-                                  NDWeight_XSec);
-  Spectrum NDSpect_select_CVXSecWeighted_Sig(NDLoader, axes.XProjection,
-                                      axes.OffAxisPosition, NDSignalCut_sel_signal,
-                                      kNoShift, NDWeight_XSec);
-  Spectrum NDSpect_select_CVXSecWeighted_WSB(NDLoader, axes.XProjection,
-                                      axes.OffAxisPosition, NDSignalCut_sel_WSB,
-                                      kNoShift, NDWeight_XSec);
-  Spectrum NDSpect_select_CVXSecWeighted_NC(NDLoader, axes.XProjection,
-                                      axes.OffAxisPosition, NDSignalCut_sel_NC,
-                                      kNoShift, NDWeight_XSec);
-
+                                         axes.OffAxisPosition, NDSignalCut_sel,
+                                         kNoShift, NDWeight_XSec);
+  Spectrum NDSpect_select_CVXSecWeighted_Sig(
+      NDLoader, axes.XProjection, axes.OffAxisPosition, NDSignalCut_sel_signal,
+      kNoShift, NDWeight_XSec);
+  Spectrum NDSpect_select_CVXSecWeighted_WSB(
+      NDLoader, axes.XProjection, axes.OffAxisPosition, NDSignalCut_sel_WSB,
+      kNoShift, NDWeight_XSec);
+  Spectrum NDSpect_select_CVXSecWeighted_NC(
+      NDLoader, axes.XProjection, axes.OffAxisPosition, NDSignalCut_sel_NC,
+      kNoShift, NDWeight_XSec);
 
   TheLoaders.Go();
 
@@ -132,20 +133,25 @@ std::string const &off_axis_file = pred.get<std::string>("input_file");
   f.WriteTObject(NearDet_signal, "NearDet_signal");
 
   TH1 *NearDet_signal_AnaWeighted = NDSpect_signal_AnaWeighted.ToTHX(POT120);
-  NearDet_signal_AnaWeighted->SetTitle(";EProxy (GeV);OffAxis Position (m);ND EvRate");
+  NearDet_signal_AnaWeighted->SetTitle(
+      ";EProxy (GeV);OffAxis Position (m);ND EvRate");
   f.WriteTObject(NearDet_signal_AnaWeighted, "NearDet_signal_AnaWeighted");
 
-  TH1 *NearDet_signal_CVXSecWeighted = NDSpect_signal_CVXSecWeighted.ToTHX(POT120);
+  TH1 *NearDet_signal_CVXSecWeighted =
+      NDSpect_signal_CVXSecWeighted.ToTHX(POT120);
   NearDet_signal_CVXSecWeighted->SetTitle(
       ";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_signal_CVXSecWeighted, "NearDet_signal_CVXSecWeighted");
+  f.WriteTObject(NearDet_signal_CVXSecWeighted,
+                 "NearDet_signal_CVXSecWeighted");
 
   TH1 *NearDet_signal_EffWeighted = NDSpect_signal_EffWeighted.ToTHX(POT120);
-  NearDet_signal_EffWeighted->SetTitle(";EProxy (GeV);OffAxis Position (m);ND EvRate");
+  NearDet_signal_EffWeighted->SetTitle(
+      ";EProxy (GeV);OffAxis Position (m);ND EvRate");
   f.WriteTObject(NearDet_signal_EffWeighted, "NearDet_signal_EffWeighted");
 
   TH1 *NearDet_signal_AllWeighted = NDSpect_signal_AllWeighted.ToTHX(POT120);
-  NearDet_signal_AllWeighted->SetTitle(";EProxy (GeV);OffAxis Position (m);ND EvRate");
+  NearDet_signal_AllWeighted->SetTitle(
+      ";EProxy (GeV);OffAxis Position (m);ND EvRate");
   f.WriteTObject(NearDet_signal_AllWeighted, "NearDet_signal_AllWeighted");
 
   TH1 *NearDet_select = NDSpect_select.ToTHX(POT120);
@@ -153,41 +159,58 @@ std::string const &off_axis_file = pred.get<std::string>("input_file");
   f.WriteTObject(NearDet_select, "NearDet_select");
 
   TH1 *NearDet_select_AnaWeighted = NDSpect_select_AnaWeighted.ToTHX(POT120);
-  NearDet_select_AnaWeighted->SetTitle(";EProxy (GeV);OffAxis Position (m);ND EvRate");
+  NearDet_select_AnaWeighted->SetTitle(
+      ";EProxy (GeV);OffAxis Position (m);ND EvRate");
   f.WriteTObject(NearDet_select_AnaWeighted, "NearDet_select_AnaWeighted");
 
-  TH1 *NearDet_select_AnaWeighted_Sig = NDSpect_select_AnaWeighted_Sig.ToTHX(POT120);
-  NearDet_select_AnaWeighted_Sig->SetTitle(";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_select_AnaWeighted_Sig, "NearDet_select_AnaWeighted_Sig");
+  TH1 *NearDet_select_AnaWeighted_Sig =
+      NDSpect_select_AnaWeighted_Sig.ToTHX(POT120);
+  NearDet_select_AnaWeighted_Sig->SetTitle(
+      ";EProxy (GeV);OffAxis Position (m);ND EvRate");
+  f.WriteTObject(NearDet_select_AnaWeighted_Sig,
+                 "NearDet_select_AnaWeighted_Sig");
 
-  TH1 *NearDet_select_AnaWeighted_WSB = NDSpect_select_AnaWeighted_WSB.ToTHX(POT120);
-  NearDet_select_AnaWeighted_WSB->SetTitle(";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_select_AnaWeighted_WSB, "NearDet_select_AnaWeighted_WSB");
+  TH1 *NearDet_select_AnaWeighted_WSB =
+      NDSpect_select_AnaWeighted_WSB.ToTHX(POT120);
+  NearDet_select_AnaWeighted_WSB->SetTitle(
+      ";EProxy (GeV);OffAxis Position (m);ND EvRate");
+  f.WriteTObject(NearDet_select_AnaWeighted_WSB,
+                 "NearDet_select_AnaWeighted_WSB");
 
-  TH1 *NearDet_select_AnaWeighted_NC = NDSpect_select_AnaWeighted_NC.ToTHX(POT120);
-  NearDet_select_AnaWeighted_NC->SetTitle(";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_select_AnaWeighted_NC, "NearDet_select_AnaWeighted_NC");
+  TH1 *NearDet_select_AnaWeighted_NC =
+      NDSpect_select_AnaWeighted_NC.ToTHX(POT120);
+  NearDet_select_AnaWeighted_NC->SetTitle(
+      ";EProxy (GeV);OffAxis Position (m);ND EvRate");
+  f.WriteTObject(NearDet_select_AnaWeighted_NC,
+                 "NearDet_select_AnaWeighted_NC");
 
-  TH1 *NearDet_select_CVXSecWeighted = NDSpect_select_CVXSecWeighted.ToTHX(POT120);
+  TH1 *NearDet_select_CVXSecWeighted =
+      NDSpect_select_CVXSecWeighted.ToTHX(POT120);
   NearDet_select_CVXSecWeighted->SetTitle(
       ";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_select_CVXSecWeighted, "NearDet_select_CVXSecWeighted");
+  f.WriteTObject(NearDet_select_CVXSecWeighted,
+                 "NearDet_select_CVXSecWeighted");
 
-  TH1 *NearDet_select_CVXSecWeighted_Sig = NDSpect_select_CVXSecWeighted_Sig.ToTHX(POT120);
+  TH1 *NearDet_select_CVXSecWeighted_Sig =
+      NDSpect_select_CVXSecWeighted_Sig.ToTHX(POT120);
   NearDet_select_CVXSecWeighted_Sig->SetTitle(
       ";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_select_CVXSecWeighted_Sig, "NearDet_select_CVXSecWeighted_Sig");
+  f.WriteTObject(NearDet_select_CVXSecWeighted_Sig,
+                 "NearDet_select_CVXSecWeighted_Sig");
 
-  TH1 *NearDet_select_CVXSecWeighted_WSB = NDSpect_select_CVXSecWeighted_WSB.ToTHX(POT120);
+  TH1 *NearDet_select_CVXSecWeighted_WSB =
+      NDSpect_select_CVXSecWeighted_WSB.ToTHX(POT120);
   NearDet_select_CVXSecWeighted_WSB->SetTitle(
       ";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_select_CVXSecWeighted_WSB, "NearDet_select_CVXSecWeighted_WSB");
+  f.WriteTObject(NearDet_select_CVXSecWeighted_WSB,
+                 "NearDet_select_CVXSecWeighted_WSB");
 
   TH1 *NearDet_select_CVXSecWeighted_NC =
       NDSpect_select_CVXSecWeighted_NC.ToTHX(POT120);
   NearDet_select_CVXSecWeighted_NC->SetTitle(
       ";EProxy (GeV);OffAxis Position (m);ND EvRate");
-  f.WriteTObject(NearDet_select_CVXSecWeighted_NC, "NearDet_select_CVXSecWeighted_NC");
+  f.WriteTObject(NearDet_select_CVXSecWeighted_NC,
+                 "NearDet_select_CVXSecWeighted_NC");
 
   f.Write();
 }
