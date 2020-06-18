2c93bda8b4a345aaf1fc02b6ba4c370473449a98
Luke Pickering <luketpickering@googlemail.com>
2020-06-18 15:05:58 -0500

    updates flux systs to include 280kA special run

--- CAFAna/PRISM/EffectiveFluxUncertaintyHelper.cxx
+++ CAFAna/PRISM/EffectiveFluxUncertaintyHelper.cxx
@@ -118,6 +118,24 @@ void EffectiveFluxUncertaintyHelper::Initialize(std::string const &filename) {
   std::string input_file = filename;
   std::string input_dir = IsCAFAnaFormat ? "" : "EffectiveFluxParameters";
 
+  TDirectory *d = f->GetDirectory(input_dir.c_str());
+
+  std::vector<std::string> param_names;
+  TList *param_names_list = nullptr;
+  d->GetObject("param_names", param_names_list);
+  if (!param_names_list) {
+    std::cout << "[ERROR]: Failed to find param_name list in TDirectory: "
+              << input_dir << " in file: " << input_file
+              << " and was passed no param_names FHiCL list to try to load."
+              << std::endl;
+    abort();
+  }
+  for (size_t p_it = 0;
+       p_it < std::min(NParams, size_t(param_names_list->GetSize())); ++p_it) {
+    param_names.push_back(
+        static_cast<TObjString *>(param_names_list->At(p_it))->String().Data());
+  }
+
   for (size_t p_it = 0; p_it < NParams; ++p_it) {
     NDTweaks.push_back(std::vector<TH1 *>());
     NDSpecHCRunTweaks.push_back(std::vector<TH1 *>());
@@ -125,7 +143,7 @@ void EffectiveFluxUncertaintyHelper::Initialize(std::string const &filename) {
 
     std::stringstream input_dir_i("");
     input_dir_i << input_dir << (input_dir.size() ? "/" : "")
-                << (IsCAFAnaFormat ? "syst" : "param_") << p_it << "/";
+                << param_names[p_it] << "/";
 
     size_t NHistsLoaded = 0;
     int nucf = kND_numu_numode;
