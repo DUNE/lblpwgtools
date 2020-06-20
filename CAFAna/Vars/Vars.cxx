6d26d90d48ca1d59961b26ac3a5c11316661ecaa
Diana Patricia Mendez <dmendezme@bnl.gov>
2020-06-19 20:05:22 -0500

    Removing dune prefixes

--- CAFAna/Vars/Vars.cxx
+++ CAFAna/Vars/Vars.cxx
@@ -12,24 +12,24 @@
 namespace ana{
 
 	// -->FD
-	const Var kRecoE_nue = SIMPLEVAR(dune.Ev_reco_nue);
-	const Var kRecoE_numu = SIMPLEVAR(dune.Ev_reco_numu);
-	const Var kRecoE_FromDep = SIMPLEVAR(dune.eRec_FromDep);
-	const Var kFDNumuPid = SIMPLEVAR(dune.cvnnumu);
-	const Var kFDNuePid = SIMPLEVAR(dune.cvnnue);
-	const Var kMVANUMU = SIMPLEVAR(dune.mvanumu);
+	const Var kRecoE_nue = SIMPLEVAR(Ev_reco_nue);
+	const Var kRecoE_numu = SIMPLEVAR(Ev_reco_numu);
+	const Var kRecoE_FromDep = SIMPLEVAR(eRec_FromDep);
+	const Var kFDNumuPid = SIMPLEVAR(cvnnumu);
+	const Var kFDNuePid = SIMPLEVAR(cvnnue);
+	const Var kMVANUMU = SIMPLEVAR(mvanumu);
 
 	// -->ND
-	const Var kRecoEnergyND = SIMPLEVAR(dune.Ev_reco);
-	const Var kRecoYND = (SIMPLEVAR(dune.Ev_reco) - SIMPLEVAR(dune.Elep_reco)) /
-	                     SIMPLEVAR(dune.Ev_reco);
+	const Var kRecoEnergyND = SIMPLEVAR(Ev_reco);
+	const Var kRecoYND = (SIMPLEVAR(Ev_reco) - SIMPLEVAR(Elep_reco)) /
+	                     SIMPLEVAR(Ev_reco);
 	const Var kRecoY_FromDep =
-	    (SIMPLEVAR(dune.eRec_FromDep) - SIMPLEVAR(dune.LepE)) /
-	    SIMPLEVAR(dune.eRec_FromDep);
+	    (SIMPLEVAR(eRec_FromDep) - SIMPLEVAR(LepE)) /
+	    SIMPLEVAR(eRec_FromDep);
 
-	const Var kTrueEnergy = SIMPLEVAR(dune.Ev);
+	const Var kTrueEnergy = SIMPLEVAR(Ev);
 
 	// CV weighting
-	const Var kCVXSecWeights = SIMPLEVAR(dune.total_xsSyst_cv_wgt); // kUnweighted
+	const Var kCVXSecWeights = SIMPLEVAR(total_xsSyst_cv_wgt); // kUnweighted
 
 }
