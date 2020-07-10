d65d648f7f3e34c1bd4747b0fa5edabd5761de33
Diana Patricia Mendez <dmendezme@bnl.gov>
2020-07-10 15:44:15 -0500

    Reduced version of cpt_joint that tries to not use common_fit_definitions and will hopefully be a clearer friendlier newby script. Work in progress.

---
+++ CAFAna/scripts/cpv_joint_optrun.C
@@ -0,0 +1,174 @@
+// TO DO:
+// * Can I generalize SetMaskHist to IChiExperiment? It's so far defined in SingleSampleExperiment
+//   experiments[expId]->SetMaskHist(0.5, (AnaV == kV4) ? 10 : 8); /// dont trust SetMaskHist. Check.
+// * Write function to load predictions from fixed location
+#pragma once
+
+#include "CAFAna/Analysis/AnalysisVersion.h"
+#include "CAFAna/Analysis/common_fit_definitions.h"
+#include "CAFAna/Analysis/Calcs.h"
+#include "CAFAna/Analysis/Exposures.h"
+//#include "CAFAna/Analysis/FitFunctionsHelper.h"
+#include "CAFAna/Experiment/ReactorExperiment.h"
+#include "CAFAna/Experiment/SingleSampleExperiment.h"
+#include "CAFAna/Experiment/SolarConstraints.h"
+
+#include "CAFAna/Systs/AnaSysts.h"
+
+using namespace ana;
+
+void cpv_joint_optrun(std::string input_predictions="input_predictions.root",
+	std::string outputFname="output_file.root",
+	std::string systSet="nosyst",
+  TString detectors="fd", TString horns="fhcrhc", TString neutrinos="nue",
+	TString penalty="nopen", std::string asimov_set="0", int hie=1,
+	double years_fhc = 3.5, double years_rhc = 3.5, int nom_exp = 7){
+
+  gROOT->SetBatch(1);
+  gRandom->SetSeed(0);
+
+  // get the analysis version
+  auto AnaV = GetAnaVersion();
+
+  // Get the systematics to use
+  std::vector<const ISyst*> systlist = GetListOfSysts(systSet);
+
+  // Oscillation parameters to use
+  std::vector<const IFitVar*> oscVars = GetOscVars("th23:th13:dmsq32", hie);
+
+  TFile* fout = new TFile(outputFname.c_str(), "RECREATE");
+  fout->cd();
+
+  double binwidth = 2*TMath::Pi()/36;
+  TGraph* gCPV = new TGraph();
+
+
+  std::vector<std::string> neutrino = {};
+  std::vector<std::string> detector = {};
+  std::vector<std::string> horn;
+  std::vector<float> pot = {};
+
+  if(neutrinos.Contains("nue")) neutrino.push_back("nue");
+  if(neutrinos.Contains("numu")) neutrino.push_back("numu");
+
+  if(detectors.Contains("nd")) detector.push_back("nd");
+  if(detectors.Contains("fd")) detector.push_back("fd");
+
+  if(horns.Contains("fhc")) horn.push_back("fhc");
+  if(horns.Contains("rhc")) horn.push_back("rhc");
+
+  const int nhorn = horn.size();
+  const int ndetector = detector.size();
+  const int nneutrino = neutrino.size();
+
+
+  ///// Predictions my way
+  ///// The joint scripts load the preds at everypoint. Avoid that. Load once.
+  std::vector<PredictionInterp*> predictions;
+  std::string fdir = "/pnfs/dune/persistent/users/dmendez/CAFAnaInputs/";
+  
+  for(int hornId=0; hornId<nhorn; hornId++){
+    for(int detId=0; detId<ndetector; detId++){
+      for(int nuId=0; nuId<nneutrino; nuId++){
+
+        std::string fname = fdir+Form("state_%s_%s.root", detector[detId].c_str(), horn[hornId].c_str());
+        std::string din = Form("%s_interp_%s_%s", detector[detId].c_str(), neutrino[nuId].c_str(), horn[hornId].c_str());
+        std::cout << "fname: " << fname << ", din: " << din << std::endl;
+
+        TFile *fin = TFile::Open(fname.c_str(), "READ");
+        predictions.push_back(LoadFrom<PredictionInterp>(fin, din).release());
+        fin->Close();
+
+        // Get nominal exposure per year from Analysis/Exposures.h
+        if(horn[hornId]=="fhc"){
+          if(detector[detId]=="nd") pot.push_back(kNDPOT[nom_exp] * years_fhc);
+          else pot.push_back(kFDPOT[nom_exp] * years_fhc);
+        } // fhc pot
+        else{
+          if(detector[detId]=="nd") pot.push_back(kNDPOT[nom_exp] * years_rhc);
+          else pot.push_back(kFDPOT[nom_exp] * years_rhc);
+        } // rhc pot
+
+      } // nuId
+    } // detId
+  } // hornId
+
+
+
+  for(double step = 0; step < 37; ++step) {
+    
+    double chisqmin = 99999;
+    double thischisq;
+
+    // fake data will be generated assuming these parameter values
+    // osc::IOscCalculatorAdjustable* calc = DefaultOscCalc();
+    double thisdcp = -TMath::Pi() + step*binwidth;
+    osc::IOscCalculatorAdjustable* trueOsc = NuFitOscCalc(hie, 1, asimov_set); 
+    trueOsc->SetdCP(thisdcp);
+
+    // defining the predictions and fake data here might seem repetitive
+    // but at least it's not reopening the prediction files at each ste
+    std::cout << "\nfilling prediction" << std::endl;
+    std::vector<Spectrum> s_predictions;
+    std::vector<Spectrum> s_fakedata;
+
+    // define the experiments for the multiexperiment
+    std::vector<const IExperiment*> experiments;
+    for(int predId=0; predId<predictions.size(); predId++){
+      s_predictions.push_back(predictions[predId]->Predict(trueOsc));
+      s_fakedata.push_back(s_predictions[predId].MockData(pot[predId],0)); // second argument = 0 defaults to random throw number
+      experiments.push_back(new SingleSampleExperiment(predictions[predId], s_fakedata[predId]));
+    }
+    if(penalty.Contains("reactor")) experiments.push_back(ReactorConstraintPDG2019());
+    if(penalty.Contains("solar")) experiments.push_back(&kSolarConstraintsPDG2019);                                                                                                                                
+    MultiExperiment multiexpt(experiments);
+
+
+    // Still need to loop over dcp choices
+    // why only have two options?
+    // TO DO: add 1/2 and 3/2 pi
+    // double idcplist = {0, 1/2, 1, 3/2}
+    // for (double idcp : idcplist) {
+    for (int idcp = 0; idcp < 2; ++idcp) {
+      double dcptest = idcp*TMath::Pi();
+      	
+      for (int ioct = -1; ioct <= 1; ioct +=2) {
+        osc::IOscCalculatorAdjustable* testOsc = NuFitOscCalc(hie, ioct, asimov_set);
+      	testOsc->SetdCP(dcptest);
+	
+      	// Make a map of seed points to try (replaces the old loops)
+      	std::map<const IFitVar*, std::vector<double>> oscSeeds = {};
+      	      	
+        SystShifts trueSyst = kNoShift;
+      	SystShifts testSyst = kNoShift;
+      	
+        MinuitFitter this_fit(&multiexpt, oscVars, systlist, MinuitFitter::kNormal);
+        double thischisq = this_fit.Fit(testOsc, testSyst, oscSeeds, {}, MinuitFitter::kVerbose)->EvalMetricVal();;
+
+//       	// thischisq = RunFitPoint(stateFname, sampleString,
+//       	// 			trueOsc, trueSyst, false,
+//       	// 			oscVars, systlist,
+//       	// 			testOsc, testSyst,
+//       	// 			oscSeeds, penalty, MinuitFitter::kNormal, nullptr);
+      	
+      	chisqmin = TMath::Min(thischisq,chisqmin);
+        std::cout << "chisqmin=" << chisqmin << std::endl;
+      }
+    }
+    
+    // chisqmin = TMath::Max(chisqmin,1e-6);
+    // gCPV->SetPoint(gCPV->GetN(),thisdcp/TMath::Pi(),TMath::Sqrt(chisqmin));
+
+    s_predictions.clear();
+    s_fakedata.clear();
+
+  } // step loop
+
+  // fout->cd();
+  // gCPV->Draw("ALP");
+  // gCPV->Write(hie > 0 ? "sens_cpv_nh" : "sens_cpv_ih");
+  // fout->Close();
+
+  predictions.clear();
+
+}
