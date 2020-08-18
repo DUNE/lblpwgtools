5750bddc4666d85734494c78c791cbf709589698
Christopher Backhouse <bckhouse@fnal.gov>
2020-08-18 12:23:52 -0500

    More indentation fixes

--- CAFAna/Prediction/PredictionExtrap.h
+++ CAFAna/Prediction/PredictionExtrap.h
@@ -10,46 +10,46 @@ namespace ana
   /// Take the output of an extrapolation and oscillate it as required
   class PredictionExtrap: public IPrediction
   {
-    public:
-      /// Takes ownership of \a extrap
-      PredictionExtrap(IExtrap* extrap);
-      virtual ~PredictionExtrap();
+  public:
+    /// Takes ownership of \a extrap
+    PredictionExtrap(IExtrap* extrap);
+    virtual ~PredictionExtrap();
 
-      // un-hide inherited method stubs so we don't get warnings from the compiler
-      using IPrediction::Predict;
-      using IPrediction::PredictComponent;
-      using IPrediction::PredictSyst;
+    // un-hide inherited method stubs so we don't get warnings from the compiler
+    using IPrediction::Predict;
+    using IPrediction::PredictComponent;
+    using IPrediction::PredictSyst;
 
-      Spectrum Predict(osc::IOscCalc* calc) const override;
-      Spectrum Predict(osc::IOscCalcStan* calc) const override;
+    Spectrum Predict(osc::IOscCalc* calc) const override;
+    Spectrum Predict(osc::IOscCalcStan* calc) const override;
 
-      Spectrum PredictComponent(osc::IOscCalc* calc,
-                                Flavors::Flavors_t flav,
-                                Current::Current_t curr,
-                                Sign::Sign_t sign) const override;
-      Spectrum PredictComponent(osc::IOscCalcStan* calc,
-                                Flavors::Flavors_t flav,
-                                Current::Current_t curr,
-                                Sign::Sign_t sign) const override;
+    Spectrum PredictComponent(osc::IOscCalc* calc,
+                              Flavors::Flavors_t flav,
+                              Current::Current_t curr,
+                              Sign::Sign_t sign) const override;
+    Spectrum PredictComponent(osc::IOscCalcStan* calc,
+                              Flavors::Flavors_t flav,
+                              Current::Current_t curr,
+                              Sign::Sign_t sign) const override;
 
-      OscillatableSpectrum ComponentCC(int from, int to) const override;
-      Spectrum ComponentNC() const override;
+    OscillatableSpectrum ComponentCC(int from, int to) const override;
+    Spectrum ComponentNC() const override;
 
     virtual void SaveTo(TDirectory* dir, const std::string& name) const override;
     static std::unique_ptr<PredictionExtrap> LoadFrom(TDirectory* dir, const std::string& name);
 
-      PredictionExtrap() = delete;
+    PredictionExtrap() = delete;
 
-      IExtrap* GetExtrap() const {return fExtrap;}
+    IExtrap* GetExtrap() const {return fExtrap;}
 
-    protected:
-      /// Templated helper function called by the non-templated versions
-      template<typename T>
-      Spectrum _PredictComponent(osc::_IOscCalc<T>* calc,
-                                 Flavors::Flavors_t flav,
-                                 Current::Current_t curr,
-                                 Sign::Sign_t sign) const;
+  protected:
+    /// Templated helper function called by the non-templated versions
+    template<typename T>
+    Spectrum _PredictComponent(osc::_IOscCalc<T>* calc,
+                               Flavors::Flavors_t flav,
+                               Current::Current_t curr,
+                               Sign::Sign_t sign) const;
 
-      IExtrap* fExtrap;
+    IExtrap* fExtrap;
   };
 }
