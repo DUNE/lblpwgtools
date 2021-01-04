5eeaa4b9485e4e6af100f2c77786799a8a9ff4b7
Ciaran Hasnip <ciaran.hasnip@physics.ox.ac.uk>
2021-01-04 06:12:48 -0600

    Improvements to ND extrapolation. Not using smart pointers for root hists in certain places as using the CAFAna implemented HistCache is more efficient. Implemented a procedure to do matching with lepton energy, more work needed on this. MC efficiency calculated as a function of true neutrino energy. This is more logical way of calculating efficiency.

--- PRISMAnalysisDefinitions.h
+++ PRISMAnalysisDefinitions.h
@@ -430,6 +430,9 @@ PRISMAxisBlob GetPRISMAxes(std::string const &varname,
                            std::string const &xbinning = "default",
                            std::string const &oabinning = "default");
 
+ana::HistAxis TrueObservable(std::string const &obsvarname = "EProxy",
+                             std::string const &binning = "uniform");
+
 extern const ana::Cut kETrueLT10GeV;
 
 extern const ana::Cut kIsOutOfTheDesert;
