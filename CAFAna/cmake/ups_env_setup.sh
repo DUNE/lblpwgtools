6b296b8503bcd979a7b78936d80a593482bbc765
Christopher Backhouse <bckhouse@fnal.gov>
2021-07-15 13:06:34 -0500

    Move to e20 compiler version, which will eventually be forced on us by osclib anyway (which is trying to keep up with sbn).

--- CAFAna/cmake/ups_env_setup.sh
+++ CAFAna/cmake/ups_env_setup.sh
@@ -8,17 +8,17 @@ if [ -z "${UPS_SHELL}" ]; then
 fi
 
 setup root v6_22_06a -q e19:prof:p383b || exit 1
-setup boost v1_73_0 -q e19:prof || exit 1
+setup boost v1_73_0 -q e20:prof || exit 1
 setup cmake v3_12_2 || exit 1
 setup jobsub_client || exit 1
 
-setup clhep v2_4_1_2 -q e19:prof || exit 1
+setup clhep v2_4_1_3a -q e20:prof || exit 1
 
 setup eigen v3_3_9a || exit
-setup stan v2_26_1 -q e19:prof || exit 1
+setup stan v2_26_1 -q e20:prof || exit 1
 
-setup osclib v00.14 -q e19:prof:n309:stan || exit 1
-setup cafanacore v01.20 -q e19:prof:n309 || exit 1
+setup osclib v00.15 -q e20:prof:n309:stan || exit 1
+setup cafanacore v01.20 -q e20:prof:n309 || exit 1
 
 # To get setup_fnal_security which helps reading files over xrootd
-setup duneutil v09_21_00 -q e19:prof
+setup duneutil v09_21_00 -q e20:prof
