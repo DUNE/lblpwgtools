d39ed7774a4cd8bbe01ebf7d47b951fb6ce4269c
Luke Pickering <luketpickering@googlemail.com>
2022-01-06 16:36:34 +0000

    Now correctly pulls in IFDHC if CAFAnaCore was built with SAM support.

--- CAFAna/cmake/FindCAFAnaCore.cmake
+++ CAFAna/cmake/FindCAFAnaCore.cmake
@@ -16,13 +16,39 @@ find_path(CAFAnaCore_LIB_DIR
   NAMES libCAFAnaCoreExt.so
   PATHS ${CAFANACORE_LIB})
 
+SET(CAFAnaCore_LIB_DIRS ${CAFAnaCore_LIB_DIR})
+SET(CAFAnaCore_INC_DIRS ${CAFAnaCore_INC_DIR})
+SET(CAFAnaCore_LIBS CAFAnaCoreExt)
+
 find_path(CAFAnaCoreSAM
   NAMES CAFAnaCore/CAFAna/Core/SAMProjectSource.h
   PATHS ${CAFANACORE_INC})
 
 SET(CAFAnaCore_CompileOptions)
 if(NOT "${CAFAnaCoreSAM}x" STREQUAL "CAFAnaCoreSAM-NOTFOUNDx")
-  SET(CAFAnaCore_CompileOptions -DWITH_SAM)
+
+  if(DEFINED ENV{IFDHC_INC})
+    SET(IFDHC_INC $ENV{IFDHC_INC})
+  endif()
+
+  if(DEFINED ENV{IFDHC_LIB})
+    SET(IFDHC_LIB $ENV{IFDHC_LIB})
+  endif()
+
+  find_path(IFDHC_INC_DIR
+    NAMES ifdh.h
+    PATHS ${IFDHC_INC}
+    REQUIRED)
+
+  find_path(IFDHC_LIB_DIR
+    NAMES libifdh.so
+    PATHS ${IFDHC_LIB}
+    REQUIRED)
+
+  LIST(APPEND CAFAnaCore_CompileOptions -DWITH_SAM)
+  LIST(APPEND CAFAnaCore_LIB_DIRS ${IFDHC_LIB_DIR})
+  LIST(APPEND CAFAnaCore_INC_DIRS ${IFDHC_INC_DIR})
+  LIST(APPEND CAFAnaCore_LIBS ifdh)
 endif()
 
 find_package_handle_standard_args(CAFAnaCore
@@ -31,8 +57,6 @@ find_package_handle_standard_args(CAFAnaCore
 
 if(CAFAnaCore_FOUND)
 
-  SET(CAFAnaCore_LIB_DIRS ${CAFAnaCore_LIB_DIR})
-  SET(CAFAnaCore_LIBS CAFAnaCoreExt)
 
   cmessage(STATUS "CAFAnaCore")
   cmessage(STATUS "     LIB_DIRS: ${CAFAnaCore_LIB_DIRS}")
@@ -42,7 +66,7 @@ if(CAFAnaCore_FOUND)
   if(NOT TARGET CAFAnaCore::Ext)
     add_library(CAFAnaCore::Ext INTERFACE IMPORTED)
     set_target_properties(CAFAnaCore::Ext PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${CAFAnaCore_INC_DIR}"
+        INTERFACE_INCLUDE_DIRECTORIES "${CAFAnaCore_INC_DIRS}"
         INTERFACE_COMPILE_OPTIONS "${CAFAnaCore_CompileOptions}"
         INTERFACE_LINK_DIRECTORIES "${CAFAnaCore_LIB_DIRS}"
         INTERFACE_LINK_LIBRARIES "${CAFAnaCore_LIBS}"
