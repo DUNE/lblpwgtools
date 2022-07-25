bee224247c773b8740f512927c28b1be1267b7cf
Christopher Backhouse <bckhouse@fnal.gov>
2022-07-25 05:33:40 -0500

    Update to new v2 of duneanaobj with all the new ND sub-objects. Update versions of everything else too.

--- CAFAna/cmake/ups_env_setup.sh
+++ CAFAna/cmake/ups_env_setup.sh
@@ -7,20 +7,22 @@ if [ -z "${UPS_SHELL}" ]; then
   exit 1
 fi
 
-setup root v6_22_08d -q e20:prof:p392 || exit 1
 setup boost v1_75_0 -q e20:prof || exit 1
-setup cmake v3_12_2 || exit 1
+setup cmake v3_22_2 || exit 1
 setup jobsub_client || exit 1
+
+setup stan v2_26_1 -q e20:prof || exit 1
 
 setup clhep v2_4_4_1 -q e20:prof || exit 1
 
-setup eigen v3_3_9a || exit
-setup stan v2_26_1 -q e20:prof || exit 1
+setup duneanaobj v02_00_01 -qe20:prof || exit 1
+setup srproxy v00.35 -q py3 || exit 1
+# There is something wrong with the ups packaging of pygcxxml. It omits the
+# "python3.9" when adding itself to $PYTHONPATH
+export PYTHONPATH=$PYGCCXML_DIR/lib/python3.9/site-packages:$PYTHONPATH
 
-setup duneanaobj v01_00_00 -qe20:gv3:prof || exit 1
-setup srproxy v00.25 -q py3 || exit 1
-setup osclib v00.17 -q e20:prof:n311:stan || exit 1
-setup cafanacore v01.26 -q e20:prof:n311 || exit 1
+setup osclib v00.21 -q e20:prof:n311:stan || exit 1
+setup cafanacore v01.29 -q e20:prof:n311 || exit 1
 
 # To get setup_fnal_security which helps reading files over xrootd
-setup duneutil v09_39_01 -q e20:prof
+setup duneutil v09_53_00d02 -q e20:prof
