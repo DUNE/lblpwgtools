9fe38403e1a306ece08d9cbec4247053e2d25c1e
Ciaran Hasnip <ciaran.hasnip@physics.ox.ac.uk>
2022-09-30 07:50:07 -0500

    Small bug fix to PRISM fitting script.

--- CAFAna/PRISM/scripts/BuildPredInterps.sh
+++ CAFAna/PRISM/scripts/BuildPredInterps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 INPUTDIR=/dune/data/users/chasnip
-OUTPUTDIR=/dune/data/users/chasnip
+OUTPUTDIR=/dune/data/users/chasnip/StateFiles_2022
 
-# NDFHCPNFSDIR=/pnfs/dune/persistent/users/abooth/Production/ND_CAFMaker/nd_offaxis/v7/CAF/Hadded/subsets/FHC
-# NDRHCPNFSDIR=/pnfs/dune/persistent/users/abooth/Production/ND_CAFMaker/nd_offaxis/v7/CAF/Hadded/subsets/RHC
+NDFHCPNFSDIR=/pnfs/dune/persistent/users/chasnip/NDCAF_OnAxisHadd/FHC
+NDRHCPNFSDIR=/pnfs/dune/persistent/users/chasnip/NDCAF_OnAxisHadd/RHC
 
 # NDFHCPNFSDIR=/root/work/software/lblpwgtools/CAFAna/inputs/ndcafs/FHC
 # NDRHCPNFSDIR=/root/work/software/lblpwgtools/CAFAna/inputs/ndcafs/RHC
@@ -12,30 +12,20 @@ OUTPUTDIR=/dune/data/users/chasnip
 #ListOSysts="allsyst"
 #ListOSysts="noxsec:nodet"
 #ListOSysts="noflux:nodet"
-ListOSysts="noxsec:noflux"
-FakeData="nominal"
-AnaBinning="default"
+#ListOSysts="noxsec:noflux"
+ListOSysts="nosyst"
+FakeData="MissingProtonFakeData_pos"
+AnaBinning="prism_default"
 AvaVar="EVisReco"
 
 #FD
-#MakePRISMPredInterps -o PRISMState_EVisReco_3.5YrFull_FDOnly.root \
-#    -F-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nonswap.root \
-#    -F-nub ${INPUTDIR}/OffAxisCAFs/FD_RHC_nonswap.root \
-#    -Fe-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nueswap.root \
-#    -Fe-nub ${INPUTDIR}/OffAxisCAFs/FD_RHC_nueswap.root \
-#    -Ft-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_tauswap.root \
-#    -Ft-nub ${INPUTDIR}/OffAxisCAFs/FD_RHC_tauswap.root \
-#    --bin-descriptor ${AnaBinning} \
-#    --syst-descriptor ${ListOSysts} \
-#    --no-fakedata-dials \
-#    -A ${AvaVar} \
-#    --PRISM-fake-data ${FakeData} \
-#    --UseSelection \
-#    -n 10000
-
-#ND nu
-CPUPROFILE=MPPI.prof MakePRISMPredInterps -o PRISMState_EVisReco_3.5YrFull_ND_nuOnly.root \
-    -N-nu "${NDFHCPNFSDIR}/CAFv7_*.root" \
+MakePRISMPredInterps -o ${OUTPUTDIR}/PRISMState_EVisReco_3.5YrFull_FDOnly.root \
+    -F-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nonswap.root \
+    -F-nub ${INPUTDIR}/OffAxisCAFs/FD_RHC_nonswap.root \
+    -Fe-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nueswap.root \
+    -Fe-nub ${INPUTDIR}/OffAxisCAFs/FD_RHC_nueswap.root \
+    -Ft-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_tauswap.root \
+    -Ft-nub ${INPUTDIR}/OffAxisCAFs/FD_RHC_tauswap.root \
     --bin-descriptor ${AnaBinning} \
     --syst-descriptor ${ListOSysts} \
     --no-fakedata-dials \
@@ -43,6 +33,17 @@ CPUPROFILE=MPPI.prof MakePRISMPredInterps -o PRISMState_EVisReco_3.5YrFull_ND_nu
     --PRISM-fake-data ${FakeData} \
     --UseSelection \
     #-n 10000
+
+#ND nu
+#CPUPROFILE=MPPI.prof MakePRISMPredInterps -o PRISMState_EVisReco_3.5YrFull_ND_nuOnly.root \
+#    -N-nu "${NDFHCPNFSDIR}/CAFv7_*.root" \
+#    --bin-descriptor ${AnaBinning} \
+#    --syst-descriptor ${ListOSysts} \
+#    --no-fakedata-dials \
+#    -A ${AvaVar} \
+#    --PRISM-fake-data ${FakeData} \
+#    --UseSelection \
+#    #-n 10000
 
 
 #ND nub
