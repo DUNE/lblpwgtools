78a52837c061a94d1026293e175da8bcce18ffd8
wilkinson-nu <callumw@fnal.gov>
2022-11-05 19:21:44 -0500

    Oops fix an obvious issue with the argument order

--- CAFAna/scripts/make_FC_throws_th13_test_fixed_seed.C
+++ CAFAna/scripts/make_FC_throws_th13_test_fixed_seed.C
@@ -28,15 +28,15 @@ int main(int argc, char const *argv[]) {
   argc--;
   char const **argv2 = &argv[1];
 
-  std::string stateFname = (argc > 1) ? argv[1] : def_stateFname;
-  std::string outputFname = (argc > 2) ? argv[2] : def_outputFname;
-  int nthrows = (argc > 3) ? atoi(argv[3]) : def_nthrows;
-  std::string systSet = (argc > 4) ? argv[4] : def_systSet;
-  std::string sampleString = (argc > 5) ? argv[5] : def_sampleString;
-  std::string throwString = (argc > 6) ? argv[6] : def_throwString;
-  int hie = (argc > 7) ? atoi(argv[7]) : def_hie;
-  std::string asimov_set = (argc > 8) ? argv[8] : def_asimov_set;
-  std::string fakeDataShift = (argc > 9) ? argv[9] : def_fakeDataShift;
+  std::string stateFname = (argc > 1) ? argv2[1] : def_stateFname;
+  std::string outputFname = (argc > 2) ? argv2[2] : def_outputFname;
+  int nthrows = (argc > 3) ? atoi(argv2[3]) : def_nthrows;
+  std::string systSet = (argc > 4) ? argv2[4] : def_systSet;
+  std::string sampleString = (argc > 5) ? argv2[5] : def_sampleString;
+  std::string throwString = (argc > 6) ? argv2[6] : def_throwString;
+  int hie = (argc > 7) ? atoi(argv2[7]) : def_hie;
+  std::string asimov_set = (argc > 8) ? argv2[8] : def_asimov_set;
+  std::string fakeDataShift = (argc > 9) ? argv2[9] : def_fakeDataShift;
 
   make_FC_throws_th13_test(stateFname, outputFname, nthrows, systSet, sampleString,
                            throwString, hie, asimov_set, fakeDataShift);
