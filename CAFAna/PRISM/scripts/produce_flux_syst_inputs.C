8a4aa3a018789720429ada1b7e686c26c866bf98
Ioana Caracas <icaracas@dunegpvm11.fnal.gov>
2023-12-04 04:23:36 -0600

    updated script to create the new flux systematics (Sept 21) -creates output file flux_shifts_OffAxis2023.root

--- CAFAna/PRISM/scripts/produce_flux_syst_inputs.C
+++ CAFAna/PRISM/scripts/produce_flux_syst_inputs.C
@@ -32,21 +32,31 @@ public:
     OffAxisPos = std::make_unique<TAxis>(NYbins, lowY, highY);
     std::cout << "Low OA = " << lowY << ", High OA = " << highY << ", N OA bins = " << NYbins << std::endl;
     std::vector<double> bin_edges;
-    for (int binx = 1; binx <= h_nom->GetXaxis()->GetNbins(); binx++) { 
+    for (int binx = 1; binx <= h_nom->GetXaxis()->GetNbins(); binx++) {
       bin_edges.emplace_back(h_nom->GetXaxis()->GetBinLowEdge(binx));
     }
     bin_edges.emplace_back(h_nom->GetXaxis()->GetBinUpEdge(h_nom->GetXaxis()->GetNbins()));
 
     for (int i = 0; i < NYbins; i++) {
-      NDnoms.emplace_back(std::make_unique<TH1D>(("hnom" + std::to_string(i)).c_str(), 
+      NDnoms.emplace_back(std::make_unique<TH1D>(("hnom" + std::to_string(i)).c_str(),
                             ("hnom"+ std::to_string(i)).c_str(), h_nom->GetXaxis()->GetNbins(),
                             &bin_edges[0])
                          );
-      NDshifts.emplace_back(std::make_unique<TH1D>(("hshift"+ std::to_string(i)).c_str(), 
+      NDshifts.emplace_back(std::make_unique<TH1D>(("hshift"+ std::to_string(i)).c_str(),
                               ("hshift"+ std::to_string(i)).c_str(), h_shift->GetXaxis()->GetNbins(),
                               &bin_edges[0])
                            );
       NDuncerts.emplace_back(nullptr);
+      // Uniform energy binning 
+      NDnomsUniformBins.emplace_back(std::make_unique<TH1D>(("hnomUniformBins"+ std::to_string(i)).c_str(),
+                             ("hnomUniformBins"+ std::to_string(i)).c_str(), h_shift->GetXaxis()->GetNbins(),
+                             &bin_edges[0])
+                          );
+
+      NDshiftsUniformBins.emplace_back(std::make_unique<TH1D>(("hshiftUniformBins"+ std::to_string(i)).c_str(),
+                             ("hshiftUniformBins"+ std::to_string(i)).c_str(), h_shift->GetXaxis()->GetNbins(),
+                             &bin_edges[0])
+                          );
     }
   }
 
@@ -54,55 +64,84 @@ public:
 
   void SortIntoTH1Ds() {
     for (int binx = 1; binx <= h_nom->GetXaxis()->GetNbins(); binx++) {
-      for (int biny = 1; biny <= h_nom->GetYaxis()->GetNbins(); biny++) { 
+      for (int biny = 1; biny <= h_nom->GetYaxis()->GetNbins(); biny++) {
         double bin_center_y = h_nom->GetYaxis()->GetBinCenter(biny);
-        int newbiny = OffAxisPos->FindFixBin(bin_center_y); 
+        int newbiny = OffAxisPos->FindFixBin(bin_center_y);
         if (newbiny > 0 && newbiny < NYbins + 1) {
           double nom_content = h_nom->GetBinContent(binx, biny);
           double shift_content = h_shift->GetBinContent(binx, biny);
           NDnoms.at(newbiny - 1)->SetBinContent(binx, nom_content);
           NDshifts.at(newbiny - 1)->SetBinContent(binx, shift_content);
-        } else { continue; }     
+        } else { continue; }
       }
     }
   }
 
+  //first rebin the original flux files in equal energy bins of 0.25 bin width
+  void RebinNDTH1DsUniformBinning(){
+    for(int i=0; i<NbinsEqual; i++){
+     EbinsEqualWidth[i] = 0 + 0.25*i;
+    }
+    EbinsEqualWidth[NbinsEqual-1] = 119.5; //last Ebin
+
+    for (int i = 0; i < NYbins; i++) {
+      NDnomsUniformBins.at(i) = std::unique_ptr<TH1D>(dynamic_cast<TH1D*>(NDnoms.at(i)->Rebin(NbinsEqual-1, ("NominalFluxUniformBins_" + std::to_string(i) ).c_str(), EbinsEqualWidth)));
+      NDshiftsUniformBins.at(i) = std::unique_ptr<TH1D>(dynamic_cast<TH1D*>(NDshifts.at(i)->Rebin(NbinsEqual-1, ("ShiftFluxUniformBins_" + std::to_string(i) ).c_str(), EbinsEqualWidth)));
+    }
+  }
+
+  //use the uniform E bins histos to calculate the desired binning
   void CalculateUncertaintyBinning(std::string flavor) {
+    double TotalErrorEvents[NYbins];
     for (int i = 0; i < NYbins; i++) {
       std::vector<double> bin_edges;
       bin_edges.emplace_back(0);
-      double sumx(0);
-      for (int binx = 1; binx <= NDnoms.at(i)->GetXaxis()->GetNbins(); binx++) {
-        sumx += NDnoms.at(i)->GetBinContent(binx);
-        if (sumx > (IsNue(flavor) ? minstats_nue : minstats_numu)) { // min stats in each bin
-          bin_edges.emplace_back(NDnoms.at(i)->GetXaxis()->GetBinLowEdge(binx));
-          sumx = 0;
+      double bin_content(0);
+
+      for (int binx = 1; binx <= NDnomsUniformBins.at(i)->GetXaxis()->GetNbins(); binx++) {
+        bin_content += NDnomsUniformBins.at(i)->GetBinContent(binx);
+
+        TotalErrorEvents[i] = 1 / sqrt(NDnomsUniformBins[i]->Integral());
+
+        if ((sqrt(bin_content) / bin_content) < PoissonErrorLimit * TotalErrorEvents[i]) { // min stats in each bin
+          bin_edges.emplace_back(NDnomsUniformBins.at(i)->GetXaxis()->GetBinLowEdge(binx));
+          bin_content = 0;
           continue;
         }
       }
+
       if (bin_edges.size() <= 0) {
-        bin_edges.emplace_back(NDnoms.at(i)->GetXaxis()->GetBinLowEdge(1));
+        bin_edges.emplace_back(NDnomsUniformBins.at(i)->GetXaxis()->GetBinLowEdge(1));
       }
-      bin_edges.emplace_back(NDnoms.at(i)->GetXaxis()
-                             ->GetBinUpEdge(NDnoms.at(i)->GetXaxis()->GetNbins()));
-      NDuncerts.at(i) = std::move(std::make_unique<TH1D>(("huncert"+ std::to_string(i)).c_str(), 
-                                    ("huncert"+ std::to_string(i)).c_str(), 
+      bin_edges.emplace_back(NDnomsUniformBins.at(i)->GetXaxis()
+                             ->GetBinUpEdge(NDnomsUniformBins.at(i)->GetXaxis()->GetNbins()));
+      NDuncerts.at(i) = std::move(std::make_unique<TH1D>(("huncert"+ std::to_string(i)).c_str(),
+                                    ("huncert"+ std::to_string(i)).c_str(),
                                     bin_edges.size() - 1, &bin_edges[0]));
+
     }
   }
 
   void CalculateUncertainty(std::string flavor) {
+    double TotalErrorEvents[NYbins];
     for (int i = 0; i < NYbins; i++) {
+      double bin_content(0);
       double nomsumx(0), shiftsumx(0);
       int sumbin(0);
-      for (int binx = 1; binx <= NDnoms.at(i)->GetXaxis()->GetNbins(); binx++) {
-        nomsumx += NDnoms.at(i)->GetBinContent(binx);
-        shiftsumx += NDshifts.at(i)->GetBinContent(binx);
-        if (nomsumx > (IsNue(flavor) ? minstats_nue : minstats_numu)) { // min stats in each bin
+      for (int binx = 1; binx <= NDnomsUniformBins.at(i)->GetXaxis()->GetNbins(); binx++) {
+        nomsumx += NDnomsUniformBins.at(i)->GetBinContent(binx);
+        shiftsumx += NDshiftsUniformBins.at(i)->GetBinContent(binx);
+
+        bin_content += NDnomsUniformBins[i]->GetBinContent(binx);
+
+	TotalErrorEvents[i] = 1 / sqrt(NDnomsUniformBins[i]->Integral());
+
+        if ((sqrt(bin_content) / bin_content) < PoissonErrorLimit * TotalErrorEvents[i]) { // min stats in each bin
           sumbin++;
           NDuncerts.at(i)->SetBinContent(sumbin, (shiftsumx - nomsumx) / nomsumx);
           nomsumx = 0;
           shiftsumx = 0;
+          bin_content = 0;
           continue;
         }
       }
@@ -121,14 +160,25 @@ public:
   TH1D *GetNDUncerts(int pos) const {
     return NDuncerts.at(pos).get();
   }
+  TH1D* GetNDNominals(int pos) const {
+    return NDnoms.at(pos).get();
+  }
+  TH1D* GetNDShifted(int pos) const {
+    return NDshifts.at(pos).get();
+  }
+  TH1D* GetNDNominalsUniformBinning(int pos) const {
+    return NDnomsUniformBins.at(pos).get();
+  }
+
 
 protected:
 
   int NYbins;
   double lowY, highY;
-
-  const double minstats_numu = 3E-5;
-  const double minstats_nue = 3E-6;
+  static const int NbinsEqual = 480; //equal number of x-bins with 0.25 width
+  double EbinsEqualWidth[NbinsEqual];
+  //binning based on percentage of events: i.e if events in an energy bin are less than 1.77% of the total events in the histogram then keep extending the bin
+  const double PoissonErrorLimit = 7.5; // this is the Poisson error limit equivalent to 1.77% events  
 
   std::unique_ptr<TAxis> OffAxisPos;
   std::unique_ptr<TH2D> h_nom;
@@ -136,6 +186,8 @@ protected:
   std::vector<std::unique_ptr<TH1D>> NDnoms;
   std::vector<std::unique_ptr<TH1D>> NDshifts;
   std::vector<std::unique_ptr<TH1D>> NDuncerts;
+  std::vector<std::unique_ptr<TH1D>> NDnomsUniformBins;
+  std::vector<std::unique_ptr<TH1D>> NDshiftsUniformBins;
 
 };
 
@@ -146,42 +198,64 @@ public:
   FDUncertaintyHelper(TH1D *h_nom_input, TH1D *h_shift_input) {
     FDnom = std::unique_ptr<TH1D>(dynamic_cast<TH1D*>(h_nom_input));
     FDshift = std::unique_ptr<TH1D>(dynamic_cast<TH1D*>(h_shift_input));
+    FDshiftOverFDnom = std::unique_ptr<TH1D>(dynamic_cast<TH1D*>(h_shift_input->Clone()));
+    FDshiftOverFDnom->Divide(dynamic_cast<TH1D*>(h_nom_input));
 
     FDuncert = nullptr;
   }
 
   ~FDUncertaintyHelper() {}
 
+  void RebinFDTH1DsUniformBinning(){
+    for(int i=0; i<NbinsEqual; i++){
+     EbinsEqualWidth[i] = 0 + 0.25*i;
+    }
+    EbinsEqualWidth[NbinsEqual-1] = 119.5; //last Ebin
+
+    FDnomUniformBins = std::unique_ptr<TH1D>( dynamic_cast<TH1D*>(FDnom->Rebin(NbinsEqual-1, "FDnomUniformBins", EbinsEqualWidth)) );
+    FDshiftUniformBins = std::unique_ptr<TH1D>( dynamic_cast<TH1D*>(FDshift->Rebin(NbinsEqual-1, "FDshiftUniformBins", EbinsEqualWidth)) );
+  }
+
   void CalculateUncertaintyBinning(std::string flavor) {
     std::vector<double> bin_edges;
     bin_edges.emplace_back(0);
-    double sumx(0);
-    for (int binx = 1; binx <= FDnom->GetXaxis()->GetNbins(); binx++) {
-      sumx += FDnom->GetBinContent(binx);
-      if (sumx > (IsNue(flavor) ? minstats_nue : minstats_numu)) { // min stats in each bin
-        bin_edges.emplace_back(FDnom->GetXaxis()->GetBinLowEdge(binx));
-        sumx = 0;
+    
+    double TotalErrorEventsFD;
+    double bin_content(0);
+    for (int binx = 1; binx <= FDnomUniformBins->GetXaxis()->GetNbins(); binx++) {
+      bin_content += FDnomUniformBins->GetBinContent(binx);
+      TotalErrorEventsFD = 1 / sqrt(FDnomUniformBins->Integral());
+      if ((sqrt(bin_content) / bin_content) < PoissonErrorLimit * TotalErrorEventsFD) { // min stats in each bin
+        bin_edges.emplace_back(FDnomUniformBins->GetXaxis()->GetBinLowEdge(binx));
+        bin_content = 0;
         continue;
       }
     }
     if (bin_edges.size() <= 0) {
-      bin_edges.emplace_back(FDnom->GetXaxis()->GetBinLowEdge(1));
+      bin_edges.emplace_back(FDnomUniformBins->GetXaxis()->GetBinLowEdge(1));
     }
-    bin_edges.emplace_back(FDnom->GetXaxis()
-                           ->GetBinUpEdge(FDnom->GetXaxis()->GetNbins()));
+    bin_edges.emplace_back(FDnomUniformBins->GetXaxis()
+                           ->GetBinUpEdge(FDnomUniformBins->GetXaxis()->GetNbins()));
     FDuncert = std::move(std::make_unique<TH1D>("FDuncert", "FDuncert",
                            bin_edges.size() - 1, &bin_edges[0]));
+   
   }
 
   void CalculateUncertainty(std::string flavor) {
     double nomsumx(0), shiftsumx(0);
+    double bin_content(0);
     int sumbin(0);
-    for (int binx = 1; binx <= FDnom->GetXaxis()->GetNbins(); binx++) {
-      nomsumx += FDnom->GetBinContent(binx);
-      shiftsumx += FDshift->GetBinContent(binx);
-      if (nomsumx > (IsNue(flavor) ? minstats_nue : minstats_numu)) { // min stats in each bin
+    double TotalErrorEventsFD;
+    for (int binx = 1; binx <= FDnomUniformBins->GetXaxis()->GetNbins(); binx++) {
+      bin_content += FDnomUniformBins->GetBinContent(binx);
+      TotalErrorEventsFD = 1 / sqrt(FDnomUniformBins->Integral());
+
+      nomsumx += FDnomUniformBins->GetBinContent(binx);
+      shiftsumx += FDshiftUniformBins->GetBinContent(binx);
+      if ((sqrt(bin_content) / bin_content) < PoissonErrorLimit * TotalErrorEventsFD){ // min stats in each bin
         sumbin++;
         FDuncert->SetBinContent(sumbin, (shiftsumx - nomsumx) / nomsumx);
+        bin_content = 0;
         nomsumx = 0;
         shiftsumx = 0;
         continue;
@@ -195,16 +269,22 @@ public:
   }
 
   TH1D *GetFDUncert() const { return FDuncert.get(); }
+  TH1D *GetFDNominal() const {return FDnom.get(); }
+  TH1D *GetFDShift() const {return FDshift.get(); }
 
 protected:
 
-  const double minstats_numu = 5E-12;
-  const double minstats_nue = 1E-13;
-
+  static const int NbinsEqual = 480; //equal number of x-bins with 0.25 width
+  double EbinsEqualWidth[NbinsEqual];
+  //binning based on percentage of events: i.e if events in an energy bin are less than 1.77% of the total events in the histogram then keep extending the bin
+  const double PoissonErrorLimit = 7.5; // this is the Poisson error limit equivalent to 1.77% events
+  
   std::unique_ptr<TH1D> FDnom;
   std::unique_ptr<TH1D> FDshift;
   std::unique_ptr<TH1D> FDuncert;
-
+  std::unique_ptr<TH1D> FDshiftOverFDnom;
+  std::unique_ptr<TH1D> FDnomUniformBins;
+  std::unique_ptr<TH1D> FDshiftUniformBins;
 
 };
 
@@ -267,21 +347,20 @@ int main(int argc, char** argv) {
   std::vector<std::string> vsign{"neutrino", "antineutrino"};
   std::vector<std::string> vspecrun{"_", "_specrun_"};
 
-  std::string outfile = ana::FindCAFAnaDir() + "/../../Systs/flux_shifts_OffAxis2022.root";
+  std::string outfile = ana::FindCAFAnaDir() + "/../../Systs/flux_shifts_OffAxis2023.root";
   std::cout << "FILE OUTPUT: " << outfile << std::endl;
   TFile *TotalUncert = TFile::Open(outfile.c_str(), "RECREATE");
 
   TAxis *OATaxis = nullptr;
 
   for (const std::string& shiftname : shift_flux_dirs) {
-    
+
     // Some string parsing
     size_t dash1 = shiftname.find("_");
     std::string syst = shiftname.substr(dash1 + 1, shiftname.find("_", dash1 + 1) - (dash1 + 1));
     std::cout << "[LOADING] " << syst << std::endl;
 
-    //OATaxis = nullptr;
-    // Creat ND flux uncertainties first
+    // Create ND flux uncertainties first
     for (const std::string& sign : vsign) {
       for (const std::string& flavor : vflavors) {
         for (const std::string& specrun : vspecrun) {
@@ -289,7 +368,7 @@ int main(int argc, char** argv) {
         std::cout << flavor << ", " << sign << std::endl;
 
         std::string write_sign = (sign == "neutrino" ? "nu" : "nubar");
-        TotalUncert->mkdir(("FluxParameters/param_" + syst + "/ND_" + 
+        TotalUncert->mkdir(("FluxParameters/param_" + syst + "/ND_" +
                             write_sign + specrun + flavor).c_str());
 
         //--------------
@@ -300,17 +379,17 @@ int main(int argc, char** argv) {
 
         std::string shift_file_ND = fluxdir + shiftname + "/" + sign + "/flux/" +
                                  "histos_g4lbne_v3r5p9_QGSP_BERT_" + shiftname +
-                                 "_" + sign + "_LAr_center.root";           
+                                 "_" + sign + "_LAr_center.root";
 
-        //std::cout << "Shift file: " << shift_file_ND << std::endl; 
+        //std::cout << "Shift file: " << shift_file_ND << std::endl;
         //std::cout << "Shift xrootd file: " << ana::pnfs2xrootd(shift_file_ND) << std::endl;
-   
+
         TFile *fnom_ND = TFile::Open(ana::pnfs2xrootd(nominal_file_ND).c_str());
 
         if (!fnom_ND || fnom_ND->IsZombie()) abort();
         TDirectory *current_dir = gDirectory;
 
-        TH2D *h_nom_ND = 
+        TH2D *h_nom_ND =
           (TH2D*)current_dir->Get(("Unosc_" + flavor + "_flux_DUNEPRISM_LAr_center").c_str());
         h_nom_ND->SetDirectory(nullptr);
 
@@ -319,7 +398,7 @@ int main(int argc, char** argv) {
         if (!fshift_ND || fshift_ND->IsZombie()) abort();
         current_dir = gDirectory;
 
-        TH2D *h_shift_ND = 
+        TH2D *h_shift_ND =
           (TH2D*)current_dir->Get(("Unosc_" + flavor + "_flux_DUNEPRISM_LAr_center").c_str());
         h_shift_ND->SetDirectory(nullptr);
 
@@ -328,34 +407,35 @@ int main(int argc, char** argv) {
 
         //----------------
         // Build ND flux uncertainties
-        TotalUncert->cd(("FluxParameters/param_" + syst + "/ND_" + 
+        TotalUncert->cd(("FluxParameters/param_" + syst + "/ND_" +
                          write_sign + specrun + flavor).c_str());
 
         OffAxisUncertaintyHandler offaxisuncert =
           OffAxisUncertaintyHandler(h_nom_ND, h_shift_ND);
         offaxisuncert.SortIntoTH1Ds();
+        offaxisuncert.RebinNDTH1DsUniformBinning();
         offaxisuncert.CalculateUncertaintyBinning(flavor);
         offaxisuncert.CalculateUncertainty(flavor);
 
         for (int pos = 0; pos < offaxisuncert.GetYNbins(); pos++) {
           TH1D *NDSliceUncert = offaxisuncert.GetNDUncerts(pos);
-          NDSliceUncert->Write(("ND_" + write_sign + specrun + flavor + "_" + 
+          NDSliceUncert->Write(("ND_" + write_sign + specrun + flavor + "_" +
                                 std::to_string(pos)).c_str());
         }
         OATaxis = new TAxis(*offaxisuncert.GetOAAxis());
         }
       }
-    }  
+    }
     // Write a TAxis of the off axis position bins for each syst param
     if (OATaxis) {
       TotalUncert->cd(("FluxParameters/param_" + syst).c_str());
-      OATaxis->Write("OffAxisTAxis");    
-    } else { 
+      OATaxis->Write("OffAxisTAxis");
+    } else {
       std::cout << "[ERROR] Not found a TAxis for position." << std::endl;
-      exit(1); 
+      exit(1);
     }
 
-    // Creat FD flux uncertainties second
+    // Create FD flux uncertainties second
     for (const std::string& sign : vsign) {
       for (const std::string& flavor : vflavors) {
 
@@ -375,7 +455,7 @@ int main(int argc, char** argv) {
         if (!fnom_FD || fnom_FD->IsZombie()) abort();
         TDirectory *current_dir = gDirectory;
 
-        TH1D *h_nom_FD = 
+        TH1D *h_nom_FD =
           (TH1D*)current_dir->Get(("Unosc_flux_" + flavor + "_finemc_DUNEFD").c_str());
         h_nom_FD->SetDirectory(nullptr);
 
@@ -384,7 +464,7 @@ int main(int argc, char** argv) {
         if (!fshift_FD || fshift_FD->IsZombie()) abort();
         current_dir = gDirectory;
 
-        TH1D *h_shift_FD = 
+        TH1D *h_shift_FD =
           (TH1D*)current_dir->Get(("Unosc_flux_" + flavor + "_finemc_DUNEFD").c_str());
         h_shift_FD->SetDirectory(nullptr);
 
@@ -395,8 +475,9 @@ int main(int argc, char** argv) {
         // Build FD flux uncertainties
         TotalUncert->cd(("FluxParameters/param_" + syst).c_str());
 
-        FDUncertaintyHelper fduncert = 
+        FDUncertaintyHelper fduncert =
           FDUncertaintyHelper(h_nom_FD, h_shift_FD);
+        fduncert.RebinFDTH1DsUniformBinning();
         fduncert.CalculateUncertaintyBinning(flavor);
         fduncert.CalculateUncertainty(flavor);
 
