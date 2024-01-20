c33ea6483381cd13b055dc4fda1cb0230fa08dd2
Maria Martinez Casales <mcasales@iastate.edu>
2024-01-20 02:14:26 -0600

    fixing comments

--- CAFAna/tute/demo0b.C
+++ CAFAna/tute/demo0b.C
@@ -21,11 +21,12 @@ using namespace ana;
 void demo0b()
 {
   // Environment variables and wildcards work. As do SAM datasets.
-  const std::string fname = "/dune/data/users/skumara/Datafiles_2x2/CAF_rootfiles/minirun4/notruth/outputCAF_notruth_*";
+  const std::string fname = "/dune/data/users/skumara/Datafiles_2x2/CAF_rootfiles/Picorun4.1/PicoRun4.1_1E17_RHC.flow*";
 
   // Source of events
   SpectrumLoader loader(fname);
 
+  /////////////////////////////////////////////////////////////////
   // Reco-Particles (SRRecoParticleProxy)  level vars and cuts
   const RecoPartVar kRecoParticleEnergy = SIMPLEPARTVAR(E);
   // Define axes for the spectra we'll make
@@ -36,20 +37,22 @@ void demo0b()
                         return abs(sr->pdg) == 13 ;
                       });
 
+  /////////////////////////////////////////////////////////////////
   // Interaction level (SRInteractionProxy)  vars 
   const Var kVtxX = SIMPLEVAR(vtx.x);
   //  we can also write vars with additional logic/operations
+  // note this particular var is actually simple so it can also be written as SIMPLEVAR(vtx.z-1300)
   const Var kVtxZOffset([](const caf::SRInteractionProxy* sr)
                       {                        
                         double z = sr-> vtx.z-1300;
                         // you could do more operations here
                         return z;
                       });
-  // note this particular var is actually simple so it can also be written as SIMPLEVAR(vtx.z-1300)
 
   //2D histaxis with interaction level variables
   const HistAxis vtxPosition( "x(cm)", Binning::Simple(70,-70,70), kVtxX, 
                               "z(cm)", Binning::Simple(70,-70,70), kVtxZOffset);
+
   // A simple selection cut at the level of vertices: i.e. containment
   const Cut kContainment([](const caf::SRInteractionProxy* sr)
                       {
@@ -65,7 +68,8 @@ void demo0b()
                         return cont;
                       });
 
-//  ~~~~**** now actually make some histograms  *****~~~~
+  ///////////////////////////////////////////////////////////////// 
+  //  ~~~~**** now actually make some histograms  *****~~~~
   // we use kNoCut at interactions level, meaning no selection on vertices, then with containment
   // You can select RecoType::kDLP,kPandora at vtx/interaction level and kPIDA as well for particles
   Spectrum sEnergyMuon(     loader.Interactions(RecoType::kDLP)[kNoCut].RecoParticles(      RecoType::kDLP)[kIsMuon], axEnergy);
@@ -81,6 +85,8 @@ void demo0b()
   const double pot = 3.5 * 1.47e21 * 40/1.13;
 
   // We are forcing the pot value because cafs dont have this information yet
+  // normally you can obtain the stored pot
+  // with spec.POT();
   sEnergyMuon.OverridePOT(pot);
   sEnergyMuonCont.OverridePOT(pot);
   sVtxPositionAll.OverridePOT(pot);
