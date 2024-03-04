29af7eeb649cc5bd560b68575e420f970724d7e8
Maria Martinez Casales <mcasales@iastate.edu>
2024-03-04 16:03:56 -0600

    Adding SR and Interaction level weights:

--- CAFAna/Core/Weight.h
+++ CAFAna/Core/Weight.h
@@ -4,11 +4,26 @@
 
 #include "duneanaobj/StandardRecord/Proxy/FwdDeclare.h"
 
+//namespace ana
+//{
+//  using Weight = _Weight<caf::SRProxy>;
+//
+//#define SIMPLEWEIGHT(CAFNAME) Weight([](const caf::SRProxy* sr){return sr->CAFNAME;})
+//
+//  const Weight kUnweighted = Unweighted<caf::SRProxy>();
+//}
+
 namespace ana
 {
-  using Weight = _Weight<caf::SRProxy>;
+  typedef _Weight<caf::SRProxy> SRWeight;
 
-#define SIMPLEWEIGHT(CAFNAME) Weight([](const caf::SRProxy* sr){return sr->CAFNAME;})
+  /// \brief Equivalent of \ref Weight acting on \ref caf::SRSpill
+  typedef _Weight<caf::SRInteractionProxy> Weight;
+  //  typedef _Weight<caf::SRTrueInteractionProxy> NuTruthWeight;
 
-  const Weight kUnweighted = Unweighted<caf::SRProxy>();
-}
+  /// The simplest possible Weight, always 1. Used as a default weight.
+  const SRWeight kSRUnweighted = Unweighted<caf::SRProxy>();
+  const Weight kUnweighted = Unweighted<caf::SRInteractionProxy>();
+
+
+} // namespace
