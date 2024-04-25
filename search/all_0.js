66079cac54698b99e56167bc2228816a78dd40f2
mcasales <mcasales@fnal.gov>
2024-04-25 19:48:11 +0000

    Deploying to gh-pages from @ DUNE/lblpwgtools@4d46467426455dc5800af93dee89ad1e9c36c496 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['making_20your_20first_20plot_20in_20cafana_0',['Making your first plot in CAFAna',['../md_CAFAna_README.html',1,'']]]
+  ['making_20your_20first_20plots_20in_20cafana_0',['Making your first plots in CAFAna',['../md_CAFAna_README.html',1,'']]]
 ];
