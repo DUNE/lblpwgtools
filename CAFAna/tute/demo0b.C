c8b276ac6abcff6dd5b51ef672595165bc84368f
Maria Martinez Casales <mcasales@iastate.edu>
2024-08-09 12:35:23 -0500

    Closer to the tutorial version, with new files and prettier plots

--- CAFAna/tute/demo0b.C
+++ CAFAna/tute/demo0b.C
@@ -12,7 +12,8 @@
 #include "TH1.h"
 #include "TH2.h"
 #include "TPad.h"
-
+#include "TLegend.h"
+#include <iostream>
 using namespace ana;
 
 // Make a few basic spectra from the cafs
@@ -24,16 +25,22 @@ using namespace ana;
 //   - True particles Level Vars/Cuts
 
 
-void demo0b()
+void demo0b( std::string option = "caf")
 {
  
-  const std::string fname ="/exp/dune/data/users/noeroy/prod/MiniRun5_1E19_RHC/MiniRun5_1E19_RHC.caf.beta1/CAF/0000000/*.root";
+  std::string fname;
+  if(option=="caf") fname = "/exp/dune/data/users/skumara/Datafiles_2x2/CAF_rootfiles/MR5_beta2a_flash/test_flash.root";
+  //"/exp/dune/data/users/noeroy/prod/MiniRun5_1E19_RHC/MiniRun5_1E19_RHC.caf.beta1/CAF/0000000/*.root";
+  // "/pnfs/dune/persistent/users/mkramer/productions/MiniRun4.5_1E19_RHC/CAF_beta3/CAF/0000000/MiniRun4.5_1E19_RHC.caf.0000";
+  if (option=="flat") fname ="/exp/dune/data/users/skumara/Datafiles_2x2/CAF_rootfiles/MR5_beta2a_flash/test_flash.flat.root";
+  // "/exp/dune/data/users/noeroy/prod/MiniRun5_1E19_RHC/MiniRun5_1E19_RHC.caf.beta1/CAF.flat/0000000/MiniRun5_1E19_RHC.caf.0000*root";
   //() still figuring out the problem with flatcafs)
-  //"/exp/dune/data/users/noeroy/prod/MiniRun5_1E19_RHC/MiniRun5_1E19_RHC.caf.beta1/CAF.flat/0000000/MiniRun5_1E19_RHC*";
+
 
   // Source of events
   SpectrumLoader loader(fname);
 
+  /////////////////////////////////////////////////////////////////
   // Reco-Particles (SRRecoParticleProxy)  level vars and cuts
   const RecoPartVar kRecoParticleEnergy = SIMPLEPARTVAR(E);
   // Define axes for the spectra we'll make
@@ -44,9 +51,9 @@ void demo0b()
                         return abs(sr->pdg) == 13 ;
                       });
 
-  // Interaction level (SRInteractionProxy)  vars 
+  /////////////////////////////////////////////////////////////////
+  // Interaction level (SRInteractionProxy)  Simple vars 
   const Var kVtxX = SIMPLEVAR(vtx.x);
-  const Var kVtxY = SIMPLEVAR(vtx.y);
   //  we can also write vars with additional logic/operations
   const Var kVtxZ([](const caf::SRInteractionProxy* sr)
                       {                        
@@ -54,19 +61,15 @@ void demo0b()
                         // you could do more operations here
                         return z;
                       });
-  // note this particular var is actually simple so it can also be written as SIMPLEVAR(vtx.z-1300)
 
   // Histaxis with interaction level variables
-  // Note you can setup a 2D axis 
+  // Note you can setup a 2D axis, you can directly write simple vars here
   const HistAxis vtxPosition( "x(cm)", Binning::Simple(70,-70,70), kVtxX, 
-                              "z(cm)", Binning::Simple(70,-70,70), kVtxZ);
+                              "z(cm)", Binning::Simple(70,-70,70), SIMPLEVAR(vtx.z)); //or kVtxZ
 
-  const HistAxis vtxPosition2("y(cm)", Binning::Simple(70,-70,70), kVtxY, 
-                              "z(cm)", Binning::Simple(70,-70,70), kVtxZ);
   // A simple selection cut at the level of vertices: i.e. containment
   const Cut kContainedVertex([](const caf::SRInteractionProxy* sr)
                       {
-
                         double x = sr->vtx.x;
                         double y = sr->vtx.y;
                         double z = sr->vtx.z;
@@ -78,7 +81,9 @@ void demo0b()
                         return cont;
                       });
 
-  ////////////// actually setup the spectra
+
+  ///////////////////////////////////////////////////////////////// 
+  //  ~~~~**** now actually make some histograms  *****~~~~
   // we use kNoCut at interactions level, meaning no selection on vertices, then with containment
   // You can select RecoType::kDLP,kPandora at vtx/interaction level and kPIDA as well for particles
   Spectrum sEnergyMuon(     loader.Interactions(RecoType::kDLP)[kNoCut].RecoParticles(      RecoType::kDLP)[kIsMuon], axEnergy);
@@ -91,14 +96,17 @@ void demo0b()
   // but your spectrumLoader points to entries from SRRecoParticle
   // Spectrum sVtxPositionCont(loader.Interactions(RecoType::kDLP)[kContainedVertex].RecoParticles(RecoType::kDLP)[kIsMuon], vtxPosition);
   
-  // try plottingwhatever to see what ge get from the indices in SRInteraction.truth
-  Spectrum sVtxPositionAll2( loader.Interactions(RecoType::kDLP)[kNoCut], vtxPosition2);
-  Spectrum sVtxPositionAllCont2( loader.Interactions(RecoType::kDLP)[kContainedVertex], vtxPosition2);
-
+   ///////////////////////////////////////////////////////////////// 
 
  const TruthVar kTrueId([](const caf::SRTrueInteractionProxy* nu){return nu->id;});
 
- const TruthCut kMyCut ([](const caf::SRTrueInteractionProxy* nu){
+  // lets try a reco histaxis wuth truth selection ...
+  const TruthCut kIsTrueNuMu ([](const caf::SRTrueInteractionProxy* nu){
+                        int pdg = nu->pdg;
+                         return pdg==14;
+    });
+
+ const TruthCut kTruthContainedVertex ([](const caf::SRTrueInteractionProxy* nu){
                         double x = nu->vtx.x;
                         double y = nu->vtx.y;
                         double z = nu->vtx.z;
@@ -115,86 +123,108 @@ void demo0b()
     int pdg = part->pdg;
     // recast in a different way 
     //(1) muon
-    if ( abs(pdg)==13) return 1;
+    if ( abs(pdg)==13) return 1.;
     //(2) electron
-    if ( abs(pdg)==11) return 2;
+    if ( abs(pdg)==11) return 2.;
     //(3) proton
-    if ( pdg==2212) return 3;
+    if ( pdg==2212) return 3.;
     // (4) neutron
-    if ( pdg==2112) return 4;
+    if ( pdg==2112) return 4.;
     // (5) pi +
-    if ( pdg==211) return 5;
+    if ( pdg==211) return 5.;
     // (6) pi -
-    if ( pdg==-211) return 6;
+    if ( pdg==-211) return 6.;
     // (7) pi0
-    if ( pdg==111) return 7;
-    else
-      return 8;
-    // (8) anything else
+    if ( pdg==111) return 7.;
+    // (8) photon
+    if (pdg==22) return 8.;
+    // (9) Any K0S, K0L K0 or K+/-
+    if (abs(pdg)==321 || pdg== 311|| pdg== 310|| pdg== 130) return 9.;
+    else{
+      //std::cout<<pdg<<std::endl;
+      return 10.;
+    }
+    // (9) anything else
   });
- std::vector<std::string> PDGlabels = {"#mu", "e","p","n","#pi^{+}","#pi^{-}","#pi^{0}","other"};
+ std::vector<std::string> PDGlabels = {"#mu", "e","p","n","#pi^{+}","#pi^{-}","#pi^{0}","#gamma","K","other"};
 
-  const TruthHistAxis myHistTruthAxis("true id", Binning::Simple(100, 0, 100), kTrueId);
   const TruthHistAxis myHistTruthAxisPDG("true pdg", Binning::Simple(100, 0, 100), SIMPLETRUTHVAR(pdg));
-  const TruthPartHistAxis myHistTruthPartAxisPDG("true particles pdg", Binning::Simple(8, 0, 8), kTruePartPDG);
+  const TruthPartHistAxis myHistTruthPartAxisPDG("true particles pdg", Binning::Simple(10, 1, 11), kTruePartPDG);
 
-  const HistAxis ixnId( "id", Binning::Simple(20,-1,19), SIMPLEVAR(id));
-  
-  Spectrum SixnId(loader.Interactions(RecoType::kDLP)[kNoCut], ixnId);
-  Spectrum SixnIdTrue(loader.NuTruths()[kNoTruthCut], myHistTruthAxis);
-  Spectrum PDGTrue(loader.NuTruths()[kNoTruthCut], myHistTruthAxisPDG);
+  ///////////////////////////////////////////////////////////////// 
+  //  ~~~~**** now  make some more histograms  *****~~~~
+  // this kind of an ugly plot
+  Spectrum PDGTrue(loader.NuTruths(), myHistTruthAxisPDG);
+  Spectrum sTrueNumuPDGtest(loader.NuTruths()[kIsTrueNuMu], myHistTruthAxisPDG);
   
   Spectrum PDGTruePrims(loader.NuTruths().TruthParticles(TruePType::kPrim)[kNoTruthPartCut], myHistTruthPartAxisPDG);
   Spectrum PDGTrueSecs(loader.NuTruths()[kNoTruthCut].TruthParticles(TruePType::kSec)[kNoTruthPartCut], myHistTruthPartAxisPDG);
   Spectrum PDGTruePreFSI(loader.NuTruths()[kNoTruthCut].TruthParticles(TruePType::kPreFSI)[kNoTruthPartCut], myHistTruthPartAxisPDG);
 
-  // lets try a reco histaxis wuth truth selection ...
-   const TruthCut kIsTrueNuMu ([](const caf::SRTrueInteractionProxy* nu){
-                        int pdg = nu->pdg;
-                         return pdg==14;
-    });
+  Spectrum sTrueVtxPositionAll( loader.NuTruths(), 
+                TruthHistAxis("Truth x(cm)", Binning::Simple(70,-70,70), SIMPLETRUTHVAR(vtx.x), 
+                              "Truth z(cm)", Binning::Simple(70,-70,70), SIMPLETRUTHVAR(vtx.z)));
+  Spectrum sTrueVtxPositionCont(loader.NuTruths()[kTruthContainedVertex], 
+                TruthHistAxis("Truth x(cm)", Binning::Simple(70,-70,70), SIMPLETRUTHVAR(vtx.x), 
+                              "Truth z(cm)", Binning::Simple(70,-70,70), SIMPLETRUTHVAR(vtx.z)));
 
+  Spectrum sTrueNumuEnergy( loader.NuTruths(), TruthHistAxis("True neutrino energy [GeV]", Binning::Simple(50,0,10),SIMPLETRUTHVAR(E)) );
+  
+  ///////////////////////////////////////////////////////////////// 
 
-  Spectrum sTrueNumuEnergy( loader.NuTruths(), TruthHistAxis("True energy [GeV]", Binning::Simple(20,0,5),SIMPLETRUTHVAR(E)) );
-  // double check that the SR cut is actually cutting the right thing 
-  Spectrum sTrueNumuPDGtest(loader.NuTruths()[kIsTrueNuMu], myHistTruthAxisPDG);
-  // Fill in the spectrum
+  // Fill in the spectra
   loader.Go();
 
+
+  // grab pot from any
   const double pot = sEnergyMuon.POT();
 
   //ta da! Draw tour histograms
-  sEnergyMuon.ToTH1(pot)->Draw("hist");
-  sEnergyMuonCont.ToTH1(pot,kMagenta+2)->Draw("hist same");
-
-
-  new TCanvas;
-  sVtxPositionAll2.ToTH2(pot)->Draw("colz");
-  new TCanvas;
-  sVtxPositionAllCont2.ToTH2(pot)->Draw("colz");
-  new TCanvas;
-  SixnId.ToTH1(pot)->Draw("hist");
-    std::cout<< " Integral of reco indices "<< SixnId.ToTH1(pot)->Integral() <<std::endl;
-  new TCanvas;
-  SixnIdTrue.ToTH1(pot,kBlue)->Draw("hist");
-  std::cout<< " Integral of true indices "<< SixnIdTrue.ToTH1(pot)->Integral() <<std::endl;
+  TH1 * emu = sEnergyMuon.ToTH1(pot); emu->Draw("hist");
+  TH1 *emuCont=sEnergyMuonCont.ToTH1(pot,kMagenta+2); emuCont->Draw("hist same");
+  auto *lege = new TLegend(.55,.5,.88,.88);    
+  lege->AddEntry(emu, "All #mu energy","L");
+  lege->AddEntry(emuCont, "Cont. #mu energy","L");
+  lege->Draw();
+  gPad->SaveAs(("demo0_sEnergyMuon"+option+".pdf").c_str());
 
   new TCanvas;
-  PDGTrue.ToTH1(pot,kBlue)->Draw("hist");
-  sTrueNumuPDGtest.ToTH1(pot,kRed)->Draw("hist same");
- new TCanvas;
+  PDGTrue.ToTH1(pot,kBlue)->Draw("hist same");
+  sTrueNumuPDGtest.ToTH1(pot,kRed, kDashed)->Draw("hist same");
+
+  new TCanvas;
+  // you can also retrieve the TH1 * object and manipulate as usual in root
   TH1* prims = PDGTruePrims.ToTH1(pot,kRed);
-  for (unsigned int i  = 0; i<PDGlabels.size(); i++)  prims->GetXaxis()->SetBinLabel(i+1,PDGlabels[i].c_str());
+  for (unsigned int i  = 0; i<PDGlabels.size(); i++)  
+  prims->GetXaxis()->SetBinLabel(i+1,PDGlabels[i].c_str());
+  prims->SetTitle("True particles ID");
   prims->Draw("hist");
-  PDGTrueSecs.ToTH1(pot,kBlue)->Draw("hist same");
-  PDGTruePreFSI.ToTH1(pot,kGreen+2)->Draw("hist same");
+  TH1* secs = PDGTrueSecs.ToTH1(pot,kBlue); secs->Draw("hist same");
+  TH1 *prefsi=  PDGTruePreFSI.ToTH1(pot,kGreen+2); prefsi->Draw("hist same");
+  auto *leg = new TLegend(.55,.7,.88,.88);    
+  leg->AddEntry(prims, "Primaries","L");
+  leg->AddEntry(secs, "Secondaries","L");
+  leg->AddEntry(prefsi, "Pre-FSI","L");
+  leg->SetFillStyle(0);
+  leg->Draw();
+  gPad->SaveAs(("demo0_PDGprims_"+option+".pdf").c_str());
 
   new TCanvas;
-  sTrueNumuEnergy.ToTH1(pot)->Draw("hist");
+  sTrueNumuEnergy.ToTH1(sTrueNumuEnergy.POT())->Draw("hist");
 
   new TCanvas;
   sVtxPositionAll.ToTH2(pot)->Draw("colz");
+  gPad->SaveAs(("demo0_vtxPosAll_"+option+".pdf").c_str());
   new TCanvas; 
   sVtxPositionCont.ToTH2(pot)->Draw("colz");
+  gPad->SaveAs(("demo0_vtxPosCont_"+option+".pdf").c_str());
+
+
+  new TCanvas;
+  sTrueVtxPositionAll.ToTH2(sTrueVtxPositionAll.POT())->Draw("colz");
+  gPad->SaveAs(("demo0_TruevtxPosAll_"+option+".pdf").c_str());
+  new TCanvas;
+  sTrueVtxPositionCont.ToTH2(sTrueVtxPositionCont.POT())->Draw("colz");
+  gPad->SaveAs(("demo0_TruevtxPosCont_"+option+".pdf").c_str());
 
 }
