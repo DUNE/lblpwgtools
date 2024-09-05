d774e618cb6d23dfdf5432f540fff0c720394333
sindhu-ku <kumaran.sindhujha@gmail.com>
2024-09-05 10:01:35 -0700

    Revert "Merge branch 'feature/source-sink-overhaul' into feature/source-sink-overhaul"
    This reverts commit b7c45251f1af09ca254c0959849961b9a2741668, reversing
    changes made to 6395c9835015f5c2946178105861ad293a1a2b2a.

--- CAFAna/cmake/ups_env_setup.sh
+++ CAFAna/cmake/ups_env_setup.sh
@@ -14,15 +14,22 @@ setup root v6_26_06b  -q e20:p3913:prof || exit 1
 setup boost v1_75_0 -q e20:prof || exit 1
 setup cmake v3_22_2 || exit 1
 setup jobsub_client || exit 1
-
-
+#setup eigen v3_3_9a || exit 1
+#setup stan v2_26_1 -q e20:prof || exit 1
+#setup osclib v00.21 -q e20:prof:n311:stan || exit 1
+#setup cafanacore v02.00 -q e20:prof:n311 || exit 1 
+#setup duneanaobj v03_02_01 -q e20:prof|| exit 1 #v03_02_01 v03_01_00
 setup fhiclcpp v4_15_03 -q e20:prof || exit 1 # for prism
 
 setup eigen v3_4_0 || exit 1
 setup stan v2_26_1 -q e20:prof || exit 1
 setup osclib testing -q e20:prof:stanfree || exit 1
 setup cafanacore testing -q e20:prof || exit 1
-setup duneanaobj v03_06_01b -q e20:prof || exit 1
+#my v
+setup duneanaobj vtestingm -q e20:debug || exit 1 
+#Jeremys v
+#setup duneanaobj vtesting -q e20:debug || exit 1 #v03_02_01 v03_01_00
+
 
 
 # To get setup_fnal_security which helps reading files over xrootd
--- CAFAna/pnfs_setup.sh
+++
@@ -1,2 +0,0 @@
-kx509
-voms-proxy-init -noregen -rfc -voms dune:/dune/Role=Analysis
