11172fe93e11982775fc07a54f83d49d3f4a4b7f
Ioana Caracas <icaracas@dunegpvm16.fnal.gov>
2024-12-03 04:12:58 -0600

    add / use the new EnuRecoFDExtrapPred analysis variable

--- CAFAna/PRISM/scripts/BuildPredInterps.sh
+++ CAFAna/PRISM/scripts/BuildPredInterps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INPUTDIR=/dune/data/users/chasnip
-OUTPUTDIR=/dune/data/users/icaracas/StateFiles/FDStateFiles
+INPUTDIR=/exp/dune/data/users/icaracas/CAFFiles/FDCAFs
+OUTPUTDIR=/exp/dune/data/users/icaracas/StateFiles/NDStateFiles
 #OUTPUTDIR=/dune/data/users/icaracas/StateFiles/NDStateFiles/NewFluxSep21Etrue/AllFluxPars
 
 NDFHCPNFSDIR=/pnfs/dune/persistent/users/chasnip/NDCAF_OnAxisHadd/FHC
@@ -14,8 +14,8 @@ NDRHCPNFSDIR=/pnfs/dune/persistent/users/chasnip/NDCAF_OnAxisHadd/RHC
 #ListOSysts="noxsec:nodet"
 #ListOSysts="noflux:nodet"
 #ListOSysts="noxsec:noflux"
-#ListOSysts="nosyst"
-ListOSysts="sept21flux:noxsec:nodet"
+ListOSysts="nosyst"
+#ListOSysts="sept21flux:noxsec:nodet"
 #ListOSysts="nov17flux:noxsec:nodet"
 #ListOSysts="nov17flux:nodet:noxsec"
 #ListOSysts="list:MissingProtonFakeData"
@@ -27,20 +27,22 @@ ListOSysts="sept21flux:noxsec:nodet"
 #ListOSysts="list:MaCCQE"
 #ListOSysts="list:CorrHadSqrt:CorrHadInvSqrt:CorrMuSqrt:CorrMuInvSqrt:CorrEMSqrt:CorrEMInvSqrt:EScaleMuLAr:ChargedHadCorr:EMCorr:RecoEnergyScaleND:RecoEnergySqrtND:RecoEnergyInvSqrtND:MuonRecoResND:EMRecoResND:ChargedHadRecoResND"
 FakeData="MissingProtonFakeData_pos"
-AnaBinning="prism_default"
-AvaVar="EnuReco"
+#AnaBinning="prism_default"
+AnaBinning="fine_prism"
+#AvaVar="EnuReco"
+AvaVar="EnuRecoFDExtrapPred"
 
 #FD
-MakePRISMPredInterps -o ${OUTPUTDIR}/FDState_OnlyNu_FHC_EnuReco_NewSep21_FluxSysts_AllFocusingParams.root \
-    -F-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nonswap.root \
-    -Fe-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nueswap.root \
-    -Ft-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_tauswap.root \
-   --bin-descriptor ${AnaBinning} \
-   --syst-descriptor ${ListOSysts} \
-    -A ${AvaVar} \
-    --PRISM-fake-data ${FakeData} \
-    --no-fakedata-dials \
-    --UseSelection \
+#MakePRISMPredInterps -o ${OUTPUTDIR}/FDState_OnlyNu_FHC_EnuReco_visEtrue_NoSysts_NDCAFsUpdated_SameBinningInFDErecAsNDErec_FDEffVsFDErec_ForTrainWithOscSpectrumi_CorrectOABinning_FDDataNoSelectionCuts.root \
+#    -F-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nonswap.root \
+#    -Fe-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_nueswap.root \
+#    -Ft-nu ${INPUTDIR}/OffAxisCAFs/FD_FHC_tauswap.root \
+#   --bin-descriptor ${AnaBinning} \
+#   --syst-descriptor ${ListOSysts} \
+#    -A ${AvaVar} \
+#    --PRISM-fake-data ${FakeData} \
+#    --no-fakedata-dials \
+#    --UseSelection \
     #-n 10000
 
 #ND nu example for making state file with CAF files from "TempListWithInputFilesForStateFiles.txt"
@@ -55,19 +57,21 @@ MakePRISMPredInterps -o ${OUTPUTDIR}/FDState_OnlyNu_FHC_EnuReco_NewSep21_FluxSys
     #-n 10000
 
 #ND nu
-#MakePRISMPredInterps -o ${OUTPUTDIR}/ND_FHC.TESTNDState.70741375.154.root \
-#    -N-nu "${NDFHCPNFSDIR}/CAFv7_*.root" \
-#    --bin-descriptor ${AnaBinning} \
-#    --syst-descriptor ${ListOSysts} \
-#    --no-fakedata-dials \
-#    -A ${AvaVar} \
-#    --PRISM-fake-data ${FakeData} \
-#    --UseSelection \
+#MakePRISMPredInterps -o ${OUTPUTDIR}/ND_FHC._EnuRecoFDExtrapPred_VisEtrue_NoSysts_CAFSFromAlexCorrected_AllEvents_sameBinsNDErecAndFDErec_noCVNcutPred.root \
+#    -N-nu "/pnfs/dune/persistent/users/awilkins/ND_CAF_fdrecopreds/chasnip-NDCAF_OnAxisHadd-FHC_fdrecopreds/CAFv7_*.root" \
+MakePRISMPredInterps -o ${OUTPUTDIR}/ND_FHC._EnuRecoFDExtrapPred_VisEtrue_NoSysts_NewMuonPairedData_nolognorm_nocausalnearmask_resample-oscEv.root \
+    -N-nu "/pnfs/dune/persistent/users/awilkins/ND_CAF_fdrecopreds/chasnip-NDCAF_OnAxisHadd-FHC_fdrecopreds_muresim_nolognorm_nocausalnearmask_resample-oscEv/CAFv7*" \
+    --bin-descriptor ${AnaBinning} \
+    --syst-descriptor ${ListOSysts} \
+    --no-fakedata-dials \
+    -A ${AvaVar} \
+    --PRISM-fake-data ${FakeData} \
+    --UseSelection \
     #-n 10000
 
 
 #ND nub
-#MakePRISMPredInterps -o ${OUTPUTDIR}/ND_RHC.TESTNDState.2797757.668.root \
+#MakePRISMPredInterps -o ${OUTPUTDIR}/ND_RHC_EnuReco_VisETrue_NoSysts_StandardTestSelection_1E4Events.root \
 #    -N-nub "${NDRHCPNFSDIR}/CAFv7_*.root" \
 #    --bin-descriptor ${AnaBinning} \
 #    --syst-descriptor ${ListOSysts} \
