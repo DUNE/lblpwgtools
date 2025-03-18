452d9c4a6fd2f627fa81ad82570816a77b9e0119
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2025-03-18 10:34:28 -0400

    some demo true vertex Vars

--- CAFAna/Vars/Vars.h
+++ CAFAna/Vars/Vars.h
@@ -1,5 +1,7 @@
-/// Eventually split this in {Nue,Numu}Vars.{cxx,h}
+// Eventually maybe we split this into truth-only TruthVars.{cxx,h}, etc.
 #pragma once
+
+#include "duneanaobj/StandardRecord/Proxy/SRProxy.h"
 
 #include "CAFAna/Core/Utilities.h"
 #include "CAFAna/Core/Var.h"
@@ -13,5 +15,13 @@ namespace ana
   // simple examples
   extern const Var kRecoEnu;
   extern const TruthVar kTrueEnu;
+
+  // useful truth-only variables
+  inline const TruthVar kTrueVtxX_NT = SIMPLETRUTHVAR(vtx.x);
+  inline const TruthVar kTrueVtxY_NT = SIMPLETRUTHVAR(vtx.y);
+  inline const TruthVar kTrueVtxZ_NT = SIMPLETRUTHVAR(vtx.z);
+  inline const Var kTrueVtxX = TruthVarToVar(kTrueVtxX_NT);
+  inline const Var kTrueVtxY = TruthVarToVar(kTrueVtxY_NT);
+  inline const Var kTrueVtxZ = TruthVarToVar(kTrueVtxZ_NT);
 
 } // namespace
