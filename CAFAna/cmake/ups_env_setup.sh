50d450f18883876cdc129cd6cabbf63ed7517335
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2025-04-10 09:39:53 -0400

    temporary builds of dependent products...

--- CAFAna/cmake/ups_env_setup.sh
+++ CAFAna/cmake/ups_env_setup.sh
@@ -2,33 +2,27 @@
 
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
-#Jeremys versions of cafanacore, osclib, duneanaobm srproxy
-#export PRODUCTS="/exp/dune/app/users/jwolcott/ups:$PRODUCTS"
+# temporary until we get updated builds of all the dependent products
+export PRODUCTS="/exp/dune/app/users/jwolcott/ups:$PRODUCTS"
 
 if [ -z "${UPS_SHELL}" ]; then
   echo "[ERROR]: Is UPS set up?"
   exit 1
 fi
-#setup root v6_28_10b -q e20:p3915:prof || exit 1 #  v6_26_06b  -q e20:p3913:prof $ 
-setup root v6_26_06b  -q e20:p3913:prof || exit 1  
+
+setup root v6_28_12  -q e20:p3915:prof || exit 1
 setup boost v1_75_0 -q e20:prof || exit 1
 setup cmake v3_22_2 || exit 1
 setup jobsub_client || exit 1
-#setup eigen v3_3_9a || exit 1
-#setup stan v2_26_1 -q e20:prof || exit 1
-#setup osclib v00.21 -q e20:prof:n311:stan || exit 1
-#setup cafanacore v02.00 -q e20:prof:n311 || exit 1 
-#setup duneanaobj v03_02_01 -q e20:prof|| exit 1 #v03_02_01 v03_01_00
 setup fhiclcpp v4_15_03 -q e20:prof || exit 1 # for prism
 
 setup eigen v3_4_0 || exit 1
 setup stan v2_26_1 -q e20:prof || exit 1
+
+# these will need to be updated as soon as we have official builds for them
 setup osclib testing -q e20:prof:stanfree || exit 1
 setup cafanacore testing -q e20:prof || exit 1
-#my v
-setup duneanaobj vtestingm -q e20:debug || exit 1 
-#Jeremys v
-#setup duneanaobj vtesting -q e20:debug || exit 1 #v03_02_01 v03_01_00
+setup duneanaobj vtesting -q e20:debug || exit 1
 
 
 
