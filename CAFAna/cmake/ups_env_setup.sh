0c9cb1da3c20a96e5a4ffa3b07d78d04538aee02
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2025-04-30 09:41:21 -0400

    tentative e26 build updates

--- CAFAna/cmake/ups_env_setup.sh
+++ CAFAna/cmake/ups_env_setup.sh
@@ -2,27 +2,31 @@
 
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
-# temporary until we get updated builds of all the dependent products
-export PRODUCTS="/exp/dune/app/users/jwolcott/ups:$PRODUCTS"
-
 if [ -z "${UPS_SHELL}" ]; then
   echo "[ERROR]: Is UPS set up?"
   exit 1
 fi
 
-setup root v6_28_12  -q e20:p3915:prof || exit 1
-setup boost v1_75_0 -q e20:prof || exit 1
 setup cmake v3_22_2 || exit 1
-setup jobsub_client || exit 1
-setup fhiclcpp v4_15_03 -q e20:prof || exit 1 # for prism
 
-setup eigen v3_4_0 || exit 1
-setup stan v2_26_1 -q e20:prof || exit 1
+setup root v6_28_12  -q e26:p3915:prof || exit 1
+setup boost v1_82_0 -q e26:prof || exit 1
 
-# these will need to be updated as soon as we have official builds for them
-setup osclib testing -q e20:prof:stanfree || exit 1
-setup cafanacore testing -q e20:prof || exit 1
-setup duneanaobj vtesting -q e20:debug || exit 1
+setup fhiclcpp v4_18_04 -q e26:prof || exit 1 # for prism
+
+setup eigen v23_08_01_66e8f || exit 1
+setup sundials v6_5_0 || exit 1
+setup stan v2_35_0a || exit 1
+
+setup osclib v00.27 -q e26:n319:prof:stan || exit 1
+
+#setup cafanacore v02.03 -q e26:n319:prof || exit 1
+
+export PRODUCTS="/exp/dune/app/users/jwolcott/ups"
+setup cafanacore testing -q e26:prof || exit 1
+
+
+setup duneanaobj v03_07_00 -q e26:prof || exit 1
 
 
 
