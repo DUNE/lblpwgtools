d7c1e58f7a4f78dabb05782cc9b5b17d8b129b28
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2026-01-24 17:12:28 +0100

    workaround for Pandora CAFs w/ MeV rather than GeV

--- lbl-extrap-cafana/lbl2025/Vars.cxx
+++ lbl-extrap-cafana/lbl2025/Vars.cxx
@@ -1,10 +1,17 @@
 #include "lbl2025/Vars.h"
 
+#include <iostream>
+#include <ostream>
+
 namespace lbl2025
 {
   const ana::Var kRecoEnu_lepCalo([](const caf::SRInteractionProxy* sri) {
-      std::cout << "I AM HERE" << std::endl;
-      std::cout << sri->Enu.calo << std::endl;
-      return sri->Enu.calo;
+      // FIXME: this needs to be fixed when Pandora CAFs have GeV rather than MeV
+      const caf::SRProxy * sr = sri->Ancestor<caf::SRProxy>();
+      auto val = sri->Enu.calo;
+      if ( !(sr->meta.fd_hd.enabled || sr->meta.fd_vd.enabled)  )
+        // std::cout << "    Enu.calo = " << sri->Enu.calo << std::endl;
+        val *= 1e-3;
+      return val;
       });
 }
