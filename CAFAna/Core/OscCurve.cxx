64ac4d2e40d350a64b1fc875506fbb19b809466d
Luiz Ricardo Prais <lrprais@dunegpvm08.fnal.gov>
2026-08-31 14:41:36 -0500

    (Porting from NOvA) For sterile neutrino oscillations, average out the probability at rapid oscillation regions of high dmsq41. Leaves the standard oscillation probability untouched. Also did kTrueEnergyBinCenters -> TrueEnergyBinCenters() to access that variable directly (although unrelated to the sterile neutrino probability averaging)

--- CAFAna/Core/OscCurve.cxx
+++ CAFAna/Core/OscCurve.cxx
@@ -3,11 +3,14 @@
 #include "CAFAna/Core/Binning.h"
 
 #include "OscLib/IOscCalc.h"
+#include "OscLib/OscCalcSterile.h"
 
 #include <cassert>
 #include <map>
+#include <iostream>
 
 #include "TH1.h"
+#include "TMath.h"
 
 namespace ana
 {
@@ -16,18 +19,37 @@ namespace ana
   template<class T> Eigen::Array<T, Eigen::Dynamic, 1>
   ToEigen(osc::_IOscCalc<T>* calc, int from, int to)
   {
-    const unsigned int N = kTrueEnergyBinCenters.size();
+    const unsigned int N = TrueEnergyBinCenters().size();
 
     // Have to allow for underflow and overflow
     Eigen::Array<T, Eigen::Dynamic, 1> ret(N+2);
     ret[0] = 0; // underflow
     ret[N+1] = (from == to || to == 0) ? 1 : 0; // overflow
 
-    const Eigen::Array<T, Eigen::Dynamic, 1> Ps = calc->P(from, to, kTrueEnergyBinCenters);
-
-    // This is clumsy, but hopefully faster than calculating oscillation probs
-    // for two dummy values.
-    for(unsigned int i = 0; i < N; ++i) ret[i+1] = Ps[i];
+    // If we're working with steriles, average over rapid dmsq41 oscillations (see docDB-15433)
+    auto scalc = dynamic_cast<osc::IOscCalcSterile*>(calc);
+    if (scalc) {
+      for (unsigned int i = 1; i <= N; ++i) {
+        double eLo = TrueEnergyBins().Edges()[i-1];
+        if (eLo <= 0) eLo = 1e-5;
+        double eHi = TrueEnergyBins().Edges()[i];
+        if (eHi <= 0) eHi = 1e-5;
+        const double xLo = scalc->GetL() / eHi;
+        const double xHi = scalc->GetL() / eLo;
+        double LOverE = 0.5 * (xLo + xHi);
+        double f = 2 * 1.27 * scalc->GetDm(4);
+        double delta = xHi - xLo;
+        double dLoE = f == 0 ? 0 : TMath::ACos(TMath::Sin(0.5*f*delta)/(0.5*f*delta))/f;
+        double eplus = scalc->GetL() / (LOverE + dLoE);
+        double eminus = scalc->GetL() / (LOverE - dLoE);
+        ret[i] = 0.5 * (scalc->P(from, to, eminus) + scalc->P(from, to, eplus));
+      } // for i
+    } else {
+      // This is clumsy, but hopefully faster than calculating oscillation probs
+      // for two dummy values.
+      const Eigen::Array<T, Eigen::Dynamic, 1> Ps = calc->P(from, to, TrueEnergyBinCenters());
+      for(unsigned int i = 0; i < N; ++i) ret[i+1] = Ps[i];
+    }
 
     return ret;
   }
@@ -35,7 +57,7 @@ namespace ana
   //----------------------------------------------------------------------
   OscCurve::OscCurve(osc::IOscCalc* calc, int from, int to)
     : Ratio(Hist::Adopt(ToEigen(calc, from, to)),
-            std::vector<Binning>(1, kTrueEnergyBins),
+            std::vector<Binning>(1, TrueEnergyBins()),
             std::vector<std::string>(1, "True Energy (GeV)")),
       fFrom(from), fTo(to)
   {
@@ -45,7 +67,7 @@ namespace ana
 #ifdef CAFANA_USE_STAN
   OscCurve::OscCurve(osc::IOscCalcStan* calc, int from, int to)
     : Ratio(Hist::AdoptStan(ToEigen(calc, from, to)),
-            std::vector<Binning>(1, kTrueEnergyBins),
+            std::vector<Binning>(1, TrueEnergyBins()),
             std::vector<std::string>(1, "True Energy (GeV)")),
       fFrom(from), fTo(to)
   {
